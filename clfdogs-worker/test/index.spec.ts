e025658704e046496ffb48370983233ce10d0edd
Ty <noreply@mail.thetyster.dev>
2024-06-14 17:52:17 -0700

    Test: Added bare minimum tests for database and API.

--- clfdogs-worker/test/index.spec.ts
+++ clfdogs-worker/test/index.spec.ts
@@ -1,25 +1,58 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { env } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
+import worker, { getFrontPageDogData } from '../src/index';
+// Import Types
+import { DogFamily } from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+const dogFamilies: DogFamily[] = [
+	{
+		groupPhoto: 'https://example.com/group_img1.jpg',
+		mother: 'Bella',
+		father: 'Max',
+		dueDate: '2023-03-13',
+		birthday: '2024-06-01',
+		goHomeDate: '2025-05-29',
+	},
+	{
+		groupPhoto: 'https://example.com/group_img2.jpg',
+		mother: 'Bella',
+		father: 'Max',
+		dueDate: '2023-01-19',
+		birthday: '2024-06-10',
+		goHomeDate: '2025-04-22',
+	},
+	{
+		groupPhoto: 'https://example.com/group_img3.jpg',
+		mother: 'Lucy',
+		father: 'Charlie',
+		dueDate: '2025-06-15',
+		birthday: null,
+		goHomeDate: null,
+	},
+	{
+		groupPhoto: 'https://example.com/group_img4.jpg',
+		mother: 'Lucy',
+		father: 'Charlie',
+		dueDate: '2025-02-15',
+		birthday: null,
+		goHomeDate: null,
+	},
+];
+
+describe('Cherry Lane Front Page API', () => {
+	it('Responds with an object containing dummy information about dogs.', async () => {
+		const request = new IncomingRequest('http://example.com/');
+		const response = await worker.fetch(request, env);
+
+    console.log(response);
+
+		expect(await response.json()).toMatchObject(dogFamilies);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('Handler function returns expected object containing dummy information.', async () => {
+		expect(await getFrontPageDogData(env)).toMatchObject(dogFamilies);
 	});
 });
--- clfdogs-worker/vitest.config.ts
+++ clfdogs-worker/vitest.config.ts
@@ -1,11 +1,24 @@
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import path from 'node:path';
+import { defineWorkersConfig, readD1Migrations } from '@cloudflare/vitest-pool-workers/config';
 
-export default defineWorkersConfig({
-  test: {
-    poolOptions: {
-      workers: {
-        wrangler: { configPath: "./wrangler.toml" },
-      },
-    },
-  },
+export default defineWorkersConfig(async () => {
+	const migrationsPath = path.join(__dirname, 'test/migrations');
+	const migrations = await readD1Migrations(migrationsPath);
+
+	return {
+		test: {
+			setupFiles: ['./test/migrateDB.ts'],
+			poolOptions: {
+				workers: {
+					singleWorker: true,
+					wrangler: {
+						configPath: './wrangler.toml',
+					},
+					miniflare: {
+						bindings: { TEST_MIGRATIONS: migrations },
+					},
+				},
+			},
+		},
+	};
 });
