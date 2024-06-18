8ea319b3fb861546cd5c9f8c4ba933a554167077
Ty <noreply@mail.thetyster.dev>
2024-06-18 15:08:55 -0700

    Refactor: Formatting, and spacing.

--- clfdogs-worker/src/index.ts
+++ clfdogs-worker/src/index.ts
@@ -4,13 +4,13 @@
 
 /* RETURNED DATA */
 export interface DogFamily {
-	/* Represents data called from dogsDb */
-	readonly groupPhoto: string;
-	readonly mother: string;
-	readonly father: string;
-	readonly dueDate: string;
-	readonly birthday: string | null;
-	readonly goHomeDate: string| null;
+  /* Represents data called from dogsDB */
+  readonly groupPhoto: string;
+  readonly mother: string;
+  readonly father: string;
+  readonly dueDate: string;
+  readonly birthday: string | null;
+  readonly goHomeDate: string | null;
 }
 
 /*****************************************************************************/
@@ -18,34 +18,34 @@ export interface DogFamily {
 /*****************************************************************************/
 
 export async function getFrontPageDogData(env: Env): Promise<DogFamily[]> {
-	const dbQuery = env.dogsDB
-		.prepare(
-			`
+  const { results } = await env.dogsDB
+    .prepare(
+      `
       SELECT
-        I.groupPhoto,
-        AM.dogName as mother,
-        AF.dogName as father,
-        L.dueDate,
-        L.birthday,
-        L.goHomeDate
+       I.groupPhoto,
+       AM.dogName as mother,
+       AF.dogName as father,
+       L.dueDate,
+       L.birthday,
+       L.goHomeDate
       FROM
       Families AS F
       LEFT JOIN Group_Photos AS I ON F.groupPhoto = I.ID
       LEFT JOIN Adults AS AM ON F.mother = AM.ID
       LEFT JOIN Adults AS AF ON F.father = AF.ID
       LEFT JOIN Litters AS L ON F.litterId = L.ID
-  `
-		)
-		.bind();
-	let { results } = await dbQuery.all<DogFamily>();
-	return results;
+
+      `
+    )
+    .bind().all<DogFamily>();
+    return results;
 }
 
 /*****************************************************************************/
 
 async function handleRootRequest(env: Env): Promise<Response> {
-	const dogs = await getFrontPageDogData(env);
-	return Response.json(dogs);
+  const dogs = await getFrontPageDogData(env);
+  return Response.json(dogs);
 }
 
 /*****************************************************************************/
@@ -53,13 +53,13 @@ async function handleRootRequest(env: Env): Promise<Response> {
 /*****************************************************************************/
 
 export default {
-	async fetch(request, env): Promise<Response> {
-		const { pathname } = new URL(request.url);
-		if (request.method === 'GET') {
-			if (pathname === '/') return handleRootRequest(env);
-			else return new Response('Not Found', { status: 404 });
-		} else {
-			return new Response('Method Not Allowed', { status: 405 });
-		}
-	},
+  async fetch(request, env): Promise<Response> {
+    const { pathname } = new URL(request.url);
+    if (request.method === 'GET') {
+      if (pathname === '/') return handleRootRequest(env);
+      else return new Response('Not Found', { status: 404 });
+    } else {
+      return new Response('Method Not Allowed', { status: 405 });
+    }
+  },
 } satisfies ExportedHandler<Env>;
