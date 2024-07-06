d48a73524ab084cadf40c3d5f5725b4525c9f3ff
Ty <noreply@mail.thetyster.dev>
2024-07-06 08:10:00 -0700

    Fix: CSS rules and quirks mode.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { type Metadata } from "next"; // {
 import { type AppProps } from "next/app"; // }
 // Styles
 import "@/styles/reset.scss";
-import "@/pages/global.scss";
+import "@/styles/global.scss";
 import { font } from "@/styles/font";
 // Components
 
@@ -15,18 +15,20 @@ export const metadata: Metadata = {
 
 export const sections = ["white", "wood", "tan"] as const;
 
-export default function CLFMain({ Component, pageProps }: AppProps): React.JSX.Element {
+export default function CLFMain({ children }): React.JSX.Element {
   return (
-    <>
-      <Head>
-          <title>Welcome to Cherry Lane Farms</title>
-      </Head>
-      <main className={font.className}>
-          <Component {...pageProps} />
-      </main>
-      <footer>
-        <p className={font.className}>&copy; {new Date().getFullYear()} Cherry Lane Farm Doodles</p>
-      </footer>
-    </>
+    <html>
+      <body>
+        <Head>
+            <title>Welcome to Cherry Lane Farms</title>
+        </Head>
+        <main className={font.className}>
+          {children}
+        </main>
+        <footer>
+          <p className={font.className}>&copy; {new Date().getFullYear()} Cherry Lane Farm Doodles</p>
+        </footer>
+      </body>
+    </html>
   );
 }
