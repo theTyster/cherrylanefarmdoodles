4dcb7893b7d71a50bb54585d3c48347f594bc739
Ty <noreply@mail.thetyster.dev>
2024-07-06 17:43:36 -0700

    Feat: Create FormLink component.

---
+++ src/components/formlink.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function FormLink({
+  children,
+  classnames = [],
+}: {
+  children: React.ReactNode;
+  classnames?: string[];
+}) {
+  return (
+    <Link
+      className={`form-link ${classnames.map((c) => c).join(" ")}`}
+      href="https://forms.zohopublic.com/cherrylanefarmsdoodles/form/Application/formperma/c1uNLpvyuDl0TdUvp1InSoINH1G-84Ugqyq-vBjiItk"
+    >
+      {children}
+    </Link>
+  );
+}
