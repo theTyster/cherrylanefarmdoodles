5ce53d71543f3f90fb609db3518149391e34a221
Ty <noreply@mail.thetyster.dev>
2024-07-11 14:59:26 -0700

    Fix: Hopefully fixes font styling issue. 🤞

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -3,7 +3,6 @@ import { Cherry_Swash } from "next/font/google";
 export const font = Cherry_Swash({
   weight: ["400", "700"],
   style: "normal",
-  preload: true,
   display: "auto",
   subsets: ["latin"],
   fallback: ["system-ui", "arial"],
