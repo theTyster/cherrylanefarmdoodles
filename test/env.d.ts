19956f92565b0d66b584a7cb8eaf43daa77b5edf
Ty <noreply@mail.thetyster.dev>
2024-07-16 13:08:50 -0700

    Test: Adds tests for R2. Integration tests with D1.

--- test/env.d.ts
+++ test/env.d.ts
@@ -1,6 +1,8 @@
 declare module 'cloudflare:test' {
-	// Controls the type of `import("cloudflare:test").env`
+  // Controls the type of `import("cloudflare:test").env`
   interface ProvidedEnv extends CloudflareEnv {
     TEST_MIGRATIONS: D1Migration[]; // Defined in `vitest.config.ts`
+    R2TestBucket: R2Bucket;
+    R2_TEST_FILES: Record<string, string>;
   }
 }
