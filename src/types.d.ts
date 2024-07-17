77e5b9d939bd3f43b883c5e8f9fb13406b969f16
Ty <noreply@mail.thetyster.dev>
2024-07-17 14:28:40 -0700

    Types: Adds types for D1 Schema.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,8 @@
+// Utility Types {
+type ArrayOf<T> = T[];
+// }
+
+// Layout Types {
 type sections = readonly ["white", "wood", "tan"];
 
 type WhiteWoodTanLayout = [
@@ -5,7 +10,9 @@ type WhiteWoodTanLayout = [
   WoodLayoutType: () => React.JSX.Element | null,
   TanLayoutType: () => React.JSX.Element | null
 ];
+// }
 
+// Data Types {
 interface DogTreeData {
   readonly groupPhoto: string;
   readonly mother: string;
@@ -25,3 +32,80 @@ interface DogTreeDataDeconstructed extends DogTreeData {
 type DogTreeDataMapObj = Record<string, number> | Record<number, string>;
 
 type DogTreeDataMap = Map<DogTreeDataMapObj, DogTreeDataDeconstructed>;
+
+// D1 {
+type D1RawTable<T = [number, string]> = T extends ArrayOf<
+  number | string | boolean
+>
+  ? T[]
+  : never;
+type D1GroupPhotosRaw = D1RawTable;
+type D1HeadshotsSmRaw = D1RawTable;
+type D1HeadshotsLgRaw = D1RawTable;
+type D1LittersRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number, //id
+    string, //dueDate
+    string, //birthday
+    number  //applicantsInQueue
+  ]
+>;
+type D1DogsRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number, //id
+    string, //gender
+    string, //noseColor
+    string, //coatColor
+    string, //personality
+    number, //headshotSmall
+    number  //headshotLarge
+  ]
+>;
+type D1AdultsRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number,  //id
+    string,  //adultName
+    string,  //breeder
+    string,  //birthday
+    string,  //eyeColor
+    boolean, //isRetired
+    string,  //about
+    number,  //weight
+    string,  //energyLevel
+    number   //dogId
+  ]
+>;
+type D1PuppiesRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number,  //id
+    string,  //puppyName
+    string,  //collarColor
+    boolean, //isAvailable
+    number,  //dogId
+    number   //litterId
+  ]
+>;
+type D1FamiliesRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number, //id
+    number, //groupPhoto
+    number, //mother
+    number, //father
+    number  //litterId
+  ]
+>;
+type D1DogToGroupPhotosRaw = D1RawTable<
+  /*prettier-ignore*/
+  [
+    number, //id
+    string, //groupPhotoId
+    number  //dogId
+  ]
+>;
+// }
+// }
