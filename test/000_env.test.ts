43d207c769f6d8923ce9e2e16d00a490b8602012
Ty <noreply@mail.thetyster.dev>
2024-07-18 14:29:45 -0700

    Test: Refactors tests to more accurately test D1/R2 integration.

--- test/000_env.test.ts
+++ test/000_env.test.ts
@@ -32,8 +32,9 @@ describe("Cloudflare Environment", async () => {
 
 describe("Backend Systems", async () => {
   const R2 = {
+    //type TestR2 = keyof typeof R2; << May need this one day?
     uploads: (await Promise.all(
-      Object.keys(env.R2_TEST_FILES).map(async (file: string) => {
+      Object.keys(env.R2_TEST_FILES).map((file: string) => {
         const res = env.dogImages.put(file, env.R2_TEST_FILES[file]);
         if (res) return res;
       })
@@ -42,171 +43,37 @@ describe("Backend Systems", async () => {
   } as const;
 
   const r2Raw = R2.uploads.reduce((acc: [string, R2Object][], cur) => {
-    const secondDimension:[string, R2Object] = [cur.key, cur];
+    const secondDimension: [string, R2Object] = [cur.key, cur];
     acc.push(secondDimension);
     return acc;
   }, []);
 
-  type TestR2 = keyof typeof R2;
-
-  describe("D1", async () => {
-    const groupPhotos = await env.dogsDB
-      .prepare("SELECT * FROM Group_Photos")
-      .bind()
-      .raw();
-
-    const headshotsSm = await env.dogsDB
-      .prepare("SELECT * FROM Headshots_Sm")
-      .bind()
-      .raw();
-
-    const headshotsLg = await env.dogsDB
-      .prepare("SELECT * FROM Headshots_Lg")
-      .bind()
-      .raw();
-
-    const families = await env.dogsDB
-      .prepare("SELECT * FROM Families")
-      .bind()
-      .raw<number[]>();
-
-    const litters = await env.dogsDB
-      .prepare("SELECT * FROM Litters")
-      .bind()
-      .raw<[number, string, string, ...any]>();
-
-    describe("Photo Data", async () => {
-      test("All group photos should be unique.", () => {
-        const groupPhotoUrls = groupPhotos.map((photo) => photo[1]);
-        const uniquePhotos = new Set(groupPhotoUrls);
-        expect(uniquePhotos.size).toBe(groupPhotoUrls.length);
-      });
-
-      test("All group photos should be in R2 Bucket.", () => {
-        expect(R2.keys.slice(0, 8)).toMatchObject(
-          groupPhotos.map((p) => p[1]).flat(1)
-        );
-      });
-
-      test("All large headshots should be unique.", () => {
-        const headshotUrls = headshotsLg.map((photo) => photo[1]);
-        const uniquePhotos = new Set(headshotUrls);
-        expect(uniquePhotos.size).toBe(headshotUrls.length);
-      });
-
-      test("All large headshots should be in R2 Bucket.", () => {
-        expect(R2.keys.slice(8, 16)).toMatchObject(
-          headshotsLg.map((p) => p[1]).flat(1)
-        );
-      });
-
-      //      test("All large headshots should correspond to an adultName", () => {
-      //        console.log(headshotsLG.forEach( photo => photo.split('_')[1] ))
-      //      });
-
-      test("All small headshots should be unique.", () => {
-        const headshotUrls = headshotsSm.map((photo) => photo[1]);
-        const uniquePhotos = new Set(headshotUrls);
-        expect(uniquePhotos.size).toBe(headshotUrls.length);
-      });
-
-      test("All small headshots should be in R2 Bucket.", () => {
-        expect(R2.keys.slice(16, 20)).toMatchObject(
-          headshotsSm.map((p) => p[1]).flat(1)
-        );
-      });
-    });
-
-    describe("Litters Data", async () => {
-      test.each(litters)("Litter ID: %s", (...litter) => {
-        expect(litter[0], "ID should be an integer.").toBeTypeOf("number");
-        expect(
-          new Date(litter[1]),
-          "Due date should be a valid date."
-        ).toBeInstanceOf(Date);
-        expect(
-          new Date(litter[2]),
-          "Birthday should be a valid date."
-        ).toBeInstanceOf(Date);
-        if (litter[3])
-          expect(
-            new Date(litter[3]),
-            "Go home date should be a valid date."
-          ).toBeInstanceOf(Date);
-        if (litter[4]) throw new Error("Litter should only have 4 columns.");
-      });
-
-      test("All litters should be unique.", () => {
-        const litterValues = litters.map((litter) => litter.slice(1, 4));
-        const litterUUIDs = litterValues.map((litter) => litter.join(""));
-        const uniqueLitters = new Set(litterUUIDs);
-        expect(uniqueLitters.size).toBe(litters.length);
-      });
-    });
-
-    describe("Families Data", async () => {
-      test.each(families)(
-        "Family ID: %s",
-        (ID, groupPhoto, mother, father, litterId, ...rest) => {
-          expect(ID, "ID should be an integer.").toBeTypeOf("number");
-          expect(groupPhoto, "Group photo should be an integer.").toBeTypeOf(
-            "number"
-          );
-          expect(mother, "Mother should be an integer.").toBeTypeOf("number");
-          expect(father, "Father should be an integer.").toBeTypeOf("number");
-          expect(litterId, "Litter ID should be an integer.").toBeTypeOf(
-            "number"
-          );
-          if (rest.length)
-            throw new Error("Family should only have 5 columns.");
-
-          //  "Litter ID should be found in Litters"
-
-          expect(
-            litters.some((litter) => litter[0] === litterId),
-            "Litter ID should be found in Litters."
-          ).toBe(true);
-        }
-      );
-
-      test("All families should be unique.", () => {
-        const familyValues = families.map((family) => family.slice(1, 5));
-        const familyUUIDs = familyValues.map((family) => family.join(""));
-        const uniqueFamilies = new Set(familyUUIDs);
-        expect(uniqueFamilies.size).toBe(families.length);
-      });
-    });
-  });
-
-  describe("R2", async () => {
+  describe("R2", () => {
     describe.each(r2Raw)(`%s`, (key, upload) => {
+      test(`is uploaded`, () => {
+        expect(key, "Did not upload").toBeTruthy();
+        expect(key, "Incorrect Key Types").toBeTypeOf("string");
+        expect(key, "Incorrect Keys").toContain(upload.key);
+      });
 
-      if (key && upload) {
-        test(`is uploaded`, () => {
-          expect(key, "Did not upload").toBeTruthy();
-          expect(key, "Incorrect Key Types").toBeTypeOf("string");
-          expect(key, "Incorrect Keys").toContain(upload.key);
-        });
+      test(`is readable`, async () => {
+        const read = await env.dogImages.get(key);
+        const meta = await env.dogImages.head(key);
+        if (!read) throw new Error(`${key} not found in R2 Bucket`);
+        if (!meta) throw new Error(`No meta information found for ${key}`);
 
-        test(`is readable`, async () => {
-          const read = await env.dogImages.get(key);
-          const meta = await env.dogImages.head(key);
-          if (!read) throw new Error(`${key} not found in R2 Bucket`);
-          if (!meta) throw new Error(`No meta information found for ${key}`);
+        const readText = await read.text(); // Must consume the body or the test will hang/fail.
 
-          const readText = await read.text(); // Must consume the body or the test will hang/fail.
+        expect(readText).toBeTruthy();
+        expect(
+          readText,
+          "Files in R2 Bucket do not match the uploads object"
+        ).toBe(env.R2_TEST_FILES[key]);
 
-          expect(readText).toBeTruthy();
-          expect(
-            readText,
-            "Files in R2 Bucket do not match the uploads object"
-          ).toBe(env.R2_TEST_FILES[key]);
-
-          expect(meta).toBeTruthy();
-          expect(meta.key).toBeTypeOf("string");
-          expect(meta.key).toBe(key);
-        });
-      } else throw new Error(`Upload failed for ${key}`);
+        expect(meta).toBeTruthy();
+        expect(meta.key).toBeTypeOf("string");
+        expect(meta.key).toBe(key);
+      });
     });
 
     test("List all files in R2 Bucket.", async () => {
@@ -217,6 +84,153 @@ describe("Backend Systems", async () => {
       expect(list.objects.length, "Not listing all uploads").toBe(
         R2.uploads.length
       );
+    });
+  });
+
+  describe("D1", async () => {
+    // Group_Photos {
+    const D1GroupPhotos = (await env.dogsDB
+      .prepare("SELECT * FROM Group_Photos")
+      .bind()
+      .raw()) as D1GroupPhotosRaw;
+    // }
+
+    // Headshots_Sm {
+    const D1HeadshotsSm = (await env.dogsDB
+      .prepare("SELECT * FROM Headshots_Sm")
+      .bind()
+      .raw()) as D1HeadshotsSmRaw;
+    // }
+
+    // Headshots_Lg {
+    const D1HeadshotsLg = (await env.dogsDB
+      .prepare("SELECT * FROM Headshots_Lg")
+      .bind()
+      .raw()) as D1HeadshotsLgRaw;
+    // }
+
+    // Litters {
+    const D1Litters = (await env.dogsDB
+      .prepare("SELECT * FROM Litters")
+      .bind()
+      .raw()) as D1LittersRaw;
+    // }
+
+    // Dogs {
+    const D1Dogs = (await env.dogsDB
+      .prepare("SELECT * FROM Dogs")
+      .bind()
+      .raw()) as D1DogsRaw;
+    // }
+
+    // Adults {
+    //    const D1Adults = (await env.dogsDB
+    //      .prepare("SELECT * FROM Adults")
+    //      .bind()
+    //      .raw()) as D1AdultsRaw;
+    //
+    //    const D1Puppies = (await env.dogsDB
+    //      .prepare("SELECT * FROM Puppies")
+    //      .bind()
+    //      .raw()) as D1PuppiesRaw;
+    // }
+
+    // Families {
+    const D1Families = (await env.dogsDB
+      .prepare("SELECT * FROM Families")
+      .bind()
+      .raw()) as D1FamiliesRaw;
+    // }
+
+    // Group Photos{
+    //    const D1DogToGroupPhotos = (await env.dogsDB
+    //      .prepare("SELECT * FROM Dog_To_Group_Photos")
+    //      .bind()
+    //      .raw()) as D1DogToGroupPhotosRaw;
+    // }
+
+    describe("Litters Data", () => {
+      describe.each(D1Litters)(
+        "Litter ID: %s",
+        (ID, dueDate, birthday, applicantsInQueue) => {
+          test(`Row: ${ID}`, () => {
+            expect(ID, "ID not an integer.").toBeTypeOf("number");
+            expect(
+              new Date(dueDate),
+              "Due date not a valid date."
+            ).toBeInstanceOf(Date);
+            if (birthday)
+              expect(
+                new Date(birthday),
+                "Birthday not a valid date."
+              ).toBeInstanceOf(Date);
+            expect(
+              applicantsInQueue,
+              "Applicants can't be negative."
+            ).toBeGreaterThanOrEqual(0);
+          });
+        }
+      );
+
+      test("All litters should be unique.", () => {
+        const litterUUIDs = D1Litters.map((litter) => litter.join(""));
+        const uniqueLitters = new Set(litterUUIDs);
+        expect(uniqueLitters.size).toBe(D1Litters.length);
+      });
+    });
+
+    describe("Dogs Data", () => {
+      describe.each(D1Dogs)("Dog ID: %s", (...rest) => {
+        const largeHeadshotID = rest[6];
+
+        test(`Has Large Headshot: ${
+          largeHeadshotID + "_" + D1HeadshotsLg[largeHeadshotID - 1][1]
+        }`, () => {
+          expect(R2.keys).toContain(
+            largeHeadshotID + "_" + D1HeadshotsLg[largeHeadshotID - 1][1]
+          );
+        });
+      });
+    });
+
+    describe("Family Data", () => {
+      describe.each(D1Families)(
+        "Family ID: %s",
+        (ID, groupPhotoID, motherID, fatherID, litterID) => {
+          test("All group photos should be in R2 Bucket.", () => {
+            const groupPhoto = D1GroupPhotos[groupPhotoID - 1]
+              .flat(1)
+              .join("_");
+            expect(R2.keys).toContain(groupPhoto);
+          });
+          test(`Row: ${ID}`, () => {
+            expect(ID, "ID should be an integer.").toBeTypeOf("number");
+            expect(
+              groupPhotoID,
+              "Group photo should be an integer."
+            ).toBeTypeOf("number");
+            expect(motherID, "Mother should be an integer.").toBeTypeOf(
+              "number"
+            );
+            expect(fatherID, "Father should be an integer.").toBeTypeOf(
+              "number"
+            );
+            expect(litterID, "Litter ID should be an integer.").toBeTypeOf(
+              "number"
+            );
+            expect(
+              D1Litters.some((litter) => litter[0] === litterID),
+              "Litter ID should be found in Litters."
+            ).toBe(true);
+          });
+        }
+      );
+    });
+
+    test("All families should be unique.", () => {
+      const familyUUIDs = D1Families.map((family) => family.join(""));
+      const uniqueFamilies = new Set(familyUUIDs);
+      expect(uniqueFamilies.size).toBe(D1Families.length);
     });
   });
 });
