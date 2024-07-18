a05e4fe13707e6e675714167787b66030a0d9844
Ty <noreply@mail.thetyster.dev>
2024-07-18 07:08:47 -0700

    Fix: DB photo naming scheme. Removes unnecessary PK autoincrcement.

--- test/migrations/0001_ins_group_photos.sql
+++ test/migrations/0001_ins_group_photos.sql
@@ -1,10 +1,10 @@
 INSERT INTO Group_Photos (groupPhoto)
   VALUES
-  ('group1.svg'),
-  ('group2.svg'),
-  ('group3.svg'),
-  ('group4.svg'),
-  ('group5.svg'),
-  ('group6.svg'),
-  ('group7.svg'),
-  ('group8.svg');
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg'),
+  ('group.svg');
