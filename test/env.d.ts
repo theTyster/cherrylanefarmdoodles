fba77185dc178bba7ffd0f7b30e77bb40ec3894e
Ty <noreply@mail.thetyster.dev>
2024-07-23 18:11:51 -0700

    Test: Correctly type R2 Bucket in Miniflare.

--- test/env.d.ts
+++ test/env.d.ts
@@ -2,7 +2,6 @@ declare module 'cloudflare:test' {
   // Controls the type of `import("cloudflare:test").env`
   interface ProvidedEnv extends CloudflareEnv {
     TEST_MIGRATIONS: D1Migration[]; // Defined in `vitest.config.ts`
-    R2TestBucket: R2Bucket;
     R2_TEST_FILES: Record<string, string>;
   }
 }
