8b510703fb065547d9c56a12691d61320eea895f
Ty <noreply@mail.thetyster.dev>
2024-08-12 18:09:36 -0700

    Types: Adds types for DogAbout component's D1 queries.

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,24 +1,25 @@
+
 // Data Types {
 interface DogTreeData {
   readonly groupPhoto: string;
   readonly mother: string;
   readonly father: string;
-  readonly dueDate: string;
+  readonly dueDate: Date;
 }
 
 type DogTreeDataMapObj = Record<string, number> | Record<number, string>;
 
 type DogTreeDataMap = Map<DogTreeDataMapObj, DogTreeDataDeconstructed>;
 type D1_TABLES<T> =
-  T extends "Group_Photos" ? D1GroupPhotos :
-  T extends "Headshots_Sm" ? D1HeadshotsSm :
-  T extends "Headshots_Lg" ? D1HeadshotsLg :
-  T extends "Litters" ? D1Litters :
-  T extends "Dogs" ? D1Dogs :
-  T extends "Adults" ? D1Adults :
-  T extends "Puppies" ? D1Puppies :
-  T extends "Families" ? D1Families :
-  T extends "Dog_To_Group_Photos" ? D1DogToGroupPhotos : never;
+  T extends "Group_Photos" ? D1GroupPhotosRaw :
+  T extends "Headshots_Sm" ? D1HeadshotsSmRaw :
+  T extends "Headshots_Lg" ? D1HeadshotsLgRaw :
+  T extends "Litters" ? D1LittersRaw :
+  T extends "Dogs" ? D1DogsRaw :
+  T extends "Adults" ? D1AdultsRaw :
+  T extends "Puppies" ? D1PuppiesRaw :
+  T extends "Families" ? D1FamiliesRaw :
+  T extends "Dog_To_Group_Photos" ? D1DogToGroupPhotosRaw : never;
 
 type D1Table<T = [number, string]> = T extends ArrayOf<
   number | string | boolean
@@ -26,23 +27,23 @@ type D1Table<T = [number, string]> = T extends ArrayOf<
   ? T[]
   : [T][];
 
-type D1GroupPhotos = [string, string];
-type D1HeadshotsSm = [string, string];
-type D1HeadshotsLg = [string, string];
-type D1Litters =
+type D1GroupPhotosRaw = [string, string];
+type D1HeadshotsSmRaw = [string, string];
+type D1HeadshotsLgRaw = [string, string];
+type D1LittersRaw =
   /**prettier-ignore*/
   [
     number, //id
-    string, //dueDate
-    string, //birthday
+    Date,   //dueDate
+    Date,   //birthday
     number  //applicantsInQueue
   ]
 ;
-type D1Dogs =
+type D1DogsRaw =
   /**prettier-ignore*/
   [
-    number, //id
-    string, //gender
+    number,    //id
+    "M" | "F", //gender
     string, //noseColor
     string, //coatColor
     string, //personality
@@ -50,22 +51,22 @@ type D1Dogs =
     string  //Headshots_Lg
   ]
 ;
-type D1Adults =
+type D1AdultsRaw =
   /**prettier-ignore*/
   [
     number,  //id
     string,  //adultName
     string,  //breeder
-    string,  //birthday
+    Date,    //birthday
     string,  //eyeColor
-    boolean, //isRetired
+    0 | 1,   //isRetired
     string,  //favActivities
     number,  //weight
     string,  //energyLevel
     number   //dogId
   ]
 ;
-type D1Puppies =
+type D1PuppiesRaw =
   /**prettier-ignore*/
   [
     number,  //id
@@ -76,7 +77,7 @@ type D1Puppies =
     number   //litterId
   ]
 ;
-type D1Families =
+type D1FamiliesRaw =
   /**prettier-ignore*/
   [
     number, //id
@@ -86,7 +87,136 @@ type D1Families =
     number  //litterId
   ]
 ;
-type D1DogToGroupPhotos =
+type D1DogToGroupPhotosRaw =
+  /**prettier-ignore*/
+  [
+    number, //id
+    string, //Group_Photos
+    number  //dogId
+  ]
+;
+// }
+interface D1GroupPhotos {
+  readonly transformUrl: string;
+  readonly hash: string;
+  readonly alt: string;
+}
+interface D1HeadshotsSm extends D1GroupPhotos{}
+interface D1HeadshotsLg extends D1GroupPhotos{}
+
+interface D1Litters {
+  readonly id: number;
+  readonly dueDate: Date;
+  readonly birthday: Date;
+  readonly applicantsInQueue: number;
+}
+
+interface D1Dogs {
+  readonly id: number;
+  readonly gender: "M" | "F";
+  readonly noseColor: string;
+  readonly coatColor: string;
+  readonly personality: string;
+  readonly Headshots_Sm: string;
+  readonly Headshots_Lg: string;
+}
+
+interface D1Adults {
+  readonly id: number;
+  readonly adultName: string;
+  readonly breeder: string;
+  readonly birthday: Date;
+  readonly eyeColor: string;
+  readonly isRetired: 0 | 1;
+  readonly favActivities: string;
+  readonly weight: number;
+  readonly energyLevel: string;
+  readonly dogId: number;
+}
+
+interface D1Puppies {
+  readonly id: number;
+  readonly puppyName: string;
+  readonly collarColor: string;
+  readonly isAvailable: boolean;
+  readonly dogId: number;
+  readonly litterId: number;
+}
+
+interface D1Families {
+  readonly id: number;
+  readonly Group_Photos: string;
+  readonly mother: number;
+  readonly father: number;
+  readonly litterId: number;
+}
+
+interface D1DogToGroupPhotos {
+  readonly id: number;
+  readonly Group_Photos: string;
+  readonly dogId: number;
+}
+
+type D1HeadshotsSmRaw = [string, string];
+type D1HeadshotsLgRaw = [string, string];
+type D1LittersRaw =
+  /**prettier-ignore*/
+  [
+    number, //id
+    Date,   //dueDate
+    Date,   //birthday
+    number  //applicantsInQueue
+  ]
+;
+type D1DogsRaw =
+  /**prettier-ignore*/
+  [
+    number,    //id
+    "M" | "F", //gender
+    string, //noseColor
+    string, //coatColor
+    string, //personality
+    string, //Headshots_Sm
+    string  //Headshots_Lg
+  ]
+;
+type D1AdultsRaw =
+  /**prettier-ignore*/
+  [
+    number,  //id
+    string,  //adultName
+    string,  //breeder
+    Date,    //birthday
+    string,  //eyeColor
+    0 | 1,   //isRetired
+    string,  //favActivities
+    number,  //weight
+    string,  //energyLevel
+    number   //dogId
+  ]
+;
+type D1PuppiesRaw =
+  /**prettier-ignore*/
+  [
+    number,  //id
+    string,  //puppyName
+    string,  //collarColor
+    boolean, //isAvailable
+    number,  //dogId
+    number   //litterId
+  ]
+;
+type D1FamiliesRaw =
+  /**prettier-ignore*/
+  [
+    number, //id
+    string, //Group_Photos
+    number, //mother
+    number, //father
+    number  //litterId
+  ]
+;
+type D1DogToGroupPhotosRaw =
   /**prettier-ignore*/
   [
     number, //id
--- src/types/dogabout.d.ts
+++ src/types/dogabout.d.ts
@@ -1,15 +1,51 @@
-interface DogAboutData {
-  adultName: string;
-  breeder: string;
-  birthday: string;
-  eyecolor: string;
-  isRetired: boolean;
-  about: string;
-  weight: string;
-  energyLevel: string;
-  gender: 'M' | 'F';
-  noseColor: string;
-  coatColor: string;
-  personality: string;
-  Headshots_Lg: string;
+interface DogAbout_DogData
+  extends Omit<D1Adults, "dogId" | "id">,
+    Pick<
+      D1Dogs,
+      | "gender"
+      | "noseColor"
+      | "coatColor"
+      | "personality"
+      | "Headshots_Lg"
+      | "Headshots_Sm",
+      Pick<D1Litters, "applicantsInQueue" | "birthday" | "dueDate">
+    > {
+  readonly adultName: D1Adults["adultName"];
+  readonly breeder: D1Adults["breeder"];
+  readonly birthday: D1Adults["birthday"];
+  readonly eyeColor: D1Adults["eyeColor"];
+  readonly isRetired: D1Adults["isRetired"];
+  readonly favActivities: D1Adults["favActivities"];
+  readonly weight: D1Adults["weight"];
+  readonly energyLevel: D1Adults["energyLevel"];
+  readonly gender: D1Dogs["gender"];
+  readonly noseColor: D1Dogs["noseColor"];
+  readonly coatColor: D1Dogs["coatColor"];
+  readonly personality: D1Dogs["personality"];
+  readonly Headshots_Lg: D1Dogs["Headshots_Lg"];
+  readonly Headshots_Sm: D1Dogs["Headshots_Sm"];
+  readonly dueDate: D1Litters["dueDate"];
+  readonly applicantsInQueue: D1Litters["applicantsInQueue"];
+  readonly litterBirthday: D1Litters["birthday"];
+}
+
+interface DogAbout_PartnerData
+  extends Pick<D1Adults, "adultName" | "breeder" | "birthday">,
+    Pick<D1Dogs, "Headshots_Sm"> {
+  readonly partnerName: D1Adults["adultName"];
+  readonly partnerBreeder: D1Adults["breeder"];
+  readonly partnerBirthday: D1Adults["birthday"];
+  readonly partnerPhoto: D1Dogs["Headshots_Sm"];
+}
+
+interface DogAboutData
+  extends DogAbout_DogData,
+    DogAbout_PartnerData,
+    Pick<
+      D1Families,
+      "Group_Photos",
+      Pick<D1Litters, "applicantsInQueue" | "birthday" | "dueDate">
+    > {
+  readonly Group_Photos: D1Families["Group_Photos"];
+  readonly dueDate: D1Litters["dueDate"];
 }
