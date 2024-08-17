fb0ade1de2981bd26c0c3a26baece6499fe3d948
Ty <noreply@mail.thetyster.dev>
2024-08-17 14:00:06 -0700

    Fix: Wrong global name import breaking types.

--- src/types/dog-tree.ts
+++ src/types/dog-tree.ts
@@ -1,7 +1,7 @@
-import { D1Columns as D1C, D1Tables as D1T } from "@/constants/data";
+import { GlobalNameSpaces as G } from "@/constants/data";
 
 export type DogTree_Litters = Pick<
         D1Litters,
-        typeof D1C.dueDate | typeof D1C.birthday | typeof D1C.applicantsInQueue
+        typeof G.dueDate | typeof G.birthday | typeof G.applicantsInQueue
       >;
 
