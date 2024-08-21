8c0f7ca462c98159ce94d92e57a1e33a275f3639
Ty <noreply@mail.thetyster.dev>
2024-08-21 05:31:00 -0700

    Refactor: Readability.

--- test/migrations/0006_ins_adults.sql
+++ test/migrations/0006_ins_adults.sql
@@ -1,10 +1,10 @@
 INSERT INTO Adults (dogId, adultName, breeder, adultBirthday, eyeColor, isRetired, favActivities, weight, energyLevel)
   VALUES
-  (1, 'Bella', 'Breeder B', '2019-02-28', 'Green', 1, 'Snuggling and sleeping.', 20, 'Medium'),
-  (2, 'Charlie', 'Breeder A', '2017-11-15', 'Blue', 0, 'Chasing squirrels and swimming.', 30, 'High'),
+/*1*/  (1, 'Bella', 'Breeder B', '2019-02-28', 'Green', 1, 'Snuggling and sleeping.', 20, 'Medium'),
+/*2*/  (2, 'Charlie', 'Breeder A', '2017-11-15', 'Blue', 0, 'Chasing squirrels and swimming.', 30, 'High'),
 
-  (7, 'Lucy', 'Breeder B', '2020-06-20', 'Brown', 0, 'Exploring and hiking.', 22, 'Medium-high'),
-  (8, 'Max', 'Breeder A', '2018-05-12', 'Brown', 0, 'Playing fetch and running.', 25, 'High'),
+/*3*/  (7, 'Lucy', 'Breeder B', '2020-06-20', 'Brown', 0, 'Exploring and hiking.', 22, 'Medium-high'),
+/*4*/  (8, 'Max', 'Breeder A', '2018-05-12', 'Brown', 0, 'Playing fetch and running.', 25, 'High'),
 
-  (9, 'Chili', 'Breeder C', '2018-05-12', 'Brown', 0, 'Playing fetch and running.', 25, 'High'),
-  (10, 'Bandit', 'Breeder A', '2018-05-12', 'Brown', 0, 'Playing fetch and running.', 25, 'High');
+/*5*/  (9, 'Chili', 'Breeder C', '2018-05-12', 'Brown', 0, 'Hiking and socializing', 25, 'High'),
+/*6*/  (10, 'Bandit', 'Breeder A', '2018-05-12', 'Brown', 0, 'Rugby and couch.', 25, 'High');
