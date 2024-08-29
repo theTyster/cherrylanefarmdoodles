1ed2dc60ff7440a5faa086725f0fd51488c67ba2
Ty <noreply@mail.thetyster.dev>
2024-08-29 11:55:13 -0700

    Fix: Column misalignment while inserting images.

--- test/migrations/0005_ins_dogs.sql
+++ test/migrations/0005_ins_dogs.sql
@@ -3,6 +3,7 @@ INSERT INTO Dogs (gender, noseColor, coat, personality, Headshots_Lg, Headshots_
 /*01 Piper      */('F', "Black", "Red tuxedo wavy coat", "A sweet, confident, social and cuddly lady. Good with children and other dogs. Piper has medium energy and a great on and off switch.", "01placeholder", "01placeholder"),
 /*02 Knox       */('M', "Brown", "Red tuxedo wavy coat", "Social, confident, loves kids. Knox has a gentle and affectionate disposition. Lower energy and a great on and off switch.", "02placeholder", "02placeholder"),
 /*03 Hailee     */('F', "Black", "Apricot and white parti, curly coat", "Fun, loving, social and confident.  Loves all dogs and humans.", "03placeholder", "03placeholder"),
+
 /*04 Purple     */('F', "Black", "Red tuxedo", NULL, "04placeholder", "04placeholder"),
 /*05 Yellow     */('F', "Black", "Red tuxedo", NULL, "05placeholder", "05placeholder"),
 /*06 Pink       */('F', "Pink", "Red with white abstract markings", NULL, "06placeholder", "06placeholder"),
--- test/migrations/0007_ins_puppies.sql
+++ test/migrations/0007_ins_puppies.sql
@@ -1,9 +1,9 @@
 INSERT INTO Puppies (dogId, puppyName, collarColor, availability, litterId)
     VALUES
-/*1*/    (4, "Benedict", "Blue", 'Adopted', 1),
-/*2*/    (5, "Colin", "Red", 'Available', 1),
-/*3*/    (6, "Daphne", "Green", 'Available', 1),
-/*4*/    (7, "Eloise", "Purple", 'Adopted', 1),
+/*1*/    (4, "Eloise", "Purple", 'Adopted', 1),
+/*2*/    (5, "Kate", "Yellow", 'Adopted', 1),
+/*3*/    (6, "Lady Whistledown", "Pink", 'Adopted', 1),
+/*4*/    (7, "Daphne", "Green", 'Available', 1),
 /*5*/    (8, "Francesca", "Nude", 'Available', 1),
-/*6*/    (9, "Kate", "Yellow", 'Adopted', 1),
-/*7*/    (10, "Lady Whistledown", "Pink", 'Adopted', 1);
+/*6*/    (9, "Benedict", "Blue", 'Adopted', 1),
+/*7*/    (10, "Colin", "Red", 'Available', 1);
