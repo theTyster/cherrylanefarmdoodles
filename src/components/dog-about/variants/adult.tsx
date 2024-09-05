240cc9a821b94a0610f97968ac82714f5b064a62
Ty <noreply@mail.thetyster.dev>
2024-09-05 16:49:33 -0700

    Feat: Adds info table to adult variant replacing list.

--- src/components/dog-about/variants/adult.tsx
+++ src/components/dog-about/variants/adult.tsx
@@ -16,100 +16,158 @@ import css from "@/styles/dog-about.module.scss";
 // Types
 import type { ParentData } from "@/types/dog-about";
 
-export default function Adult({D}: {D: ParentData}) {
-    if (!D.partnerData) throw new Error("No partner data provided.");
-    const partnerMorf = MorF(D.partnerData[G.gender]);
-    return (
-      <>
-        <div className={css.dogTitle}>
-          <h1 className={css.dogName}>
-            Meet {D.dogData[G.adultName]}
-          </h1>
-          <hr />
-          <h2 className={css.breeder}>
-            <BreederLine breeder={D.dogData[G.breeder]} />
-          </h2>
+export default function Adult({ D }: { D: ParentData }) {
+  if (!D.partnerData) throw new Error("No partner data provided.");
+  const partnerMorf = MorF(D.partnerData[G.gender]);
+  return (
+    <>
+      <div className={css.dogTitle}>
+        <h1 className={css.dogName}>Meet {D.dogData[G.adultName]}</h1>
+        <hr />
+        <h2 className={css.breeder}>
+          <BreederLine breeder={D.dogData[G.breeder]} />
+        </h2>
+      </div>
+      <article className={css.mainDog}>
+        <div className={css.attentionGetter}>
+          <LargeHeadshot
+            variant={G.Headshots_Lg}
+            id={css.Headshots_Lg}
+            src={D.dogData[G.Headshots_Lg]}
+            alt={D.dogData[G.adultName]}
+            gender={D.dogData[G.gender]}
+          />
+          <table className={css.dogInfoList}>
+            <colgroup>
+              <col span={1} className={css.col1} />
+              <col span={1} className={css.col2} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th
+                  colSpan={2}
+                  style={{ color: css.tertiaryCherry, lineHeight: "1.3em" }}
+                >
+                  {D.litterData[G.litterBirthday]
+                    ? `${D.dogData.adultName} has ${
+                        D.litterData[G.availablePuppies]
+                      } puppies available!`
+                    : `Due on ${normalizeEpochDate(
+                        D.litterData[G.dueDate].toLocaleDateString()
+                      ).replace(/.at.*/, "")}`}
+                  {D.litterData[G.dueDate].toISOString().split("T")[0] ===
+                  new Date().toISOString().split("T")[0]
+                    ? `...That's today!!`
+                    : undefined}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <b>Age:</b>
+                </td>
+                <td>
+                  {calcAge(D.dogData[G.adultBirthday].toString())} years old
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Weight:</b>
+                </td>
+                <td>{D.dogData[G.weight]}</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Energy Level:</b>
+                </td>
+                <td>{D.dogData[G.energyLevel]}</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Eye Color:</b>
+                </td>
+                <td>{D.dogData[G.eyeColor]}</td>
+              </tr>
+              {D.dogData[G.favActivities] ? (
+                <tr>
+                  <td>
+                    <b>Favorite Activities:</b>
+                  </td>
+                  <td>{D.dogData[G.favActivities]}</td>
+                </tr>
+              ) : undefined
+              }
+              <tr>
+                <td>
+                  <b>Nose Color:</b>
+                </td>
+                <td>{D.dogData[G.noseColor]}</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>Coat Color:</b>
+                </td>
+                <td>{D.dogData[G.coat]}</td>
+              </tr>
+              {D.dogData[G.personality] ? (
+                <tr>
+                  <td>
+                    <b>Personality:</b>
+                  </td>
+                  <td>{D.dogData[G.personality]}</td>
+                </tr>
+              ) : undefined}
+            </tbody>
+          </table>
         </div>
-        <article className={css.mainDog}>
-          <div className={css.attentionGetter}>
-            <LargeHeadshot
-              variant={G.Headshots_Lg}
-              id={css.Headshots_Lg}
-              src={D.dogData[G.Headshots_Lg]}
-              alt={D.dogData[G.adultName]}
-              gender={D.dogData[G.gender]}
+        <div className={css.partnerData}>
+          <div className={css.partnerVisuals}>
+            <h3 className={css.partnerName}>
+              {D.litterData[G.litterBirthday]
+                ? `Previously matched with ${D.partnerData[G.adultName]}.`
+                : `Currently matched with ${D.partnerData[G.adultName]}.`}
+            </h3>
+            <h4 className={css.partnerLastLitter}>
+              {D.litterData[G.litterBirthday]
+                ? `Last litter born on ${normalizeEpochDate(
+                    D.litterData[G.litterBirthday]!.toLocaleDateString()
+                  ).replace(/.at.*/, "")}`
+                : `Next litter due on ${normalizeEpochDate(
+                    D.litterData[G.dueDate].toLocaleDateString()
+                  ).replace(/.at.*/, "")}`}
+              {D.litterData[G.dueDate].toISOString().split("T")[0] ===
+              new Date().toISOString().split("T")[0]
+                ? `...That's today!!`
+                : undefined}
+            </h4>
+            <GroupPhoto
+              id={css[D1T.Group_Photos]}
+              src={D.ids[D1T.Group_Photos]}
+              alt={`${D.dogData[G.adultName]} and ${
+                D.partnerData[G.adultName]
+              }'s last litter.'`}
+              litterId={D.ids[G.litterId]}
             />
-            {
-              //prettier-ignore
-              <ul className={css.dogInfoList}>
-            <li className={css.adultBirthday}>                <b>Age:</b> {calcAge(D.dogData[G.adultBirthday].toString())} years old         </li>
-            <li className={css.applicantsInQueue}> <b>Puppies Available:</b> {D.litterData[G.availablePuppies]}/{D.litterData[G.totalPuppies]}</li>
-            <li className={css.favActivities}><b>Favorite Activities:</b> {D.dogData[G.favActivities]}                                            </li>
-            <li className={css.weight}>                    <b>Weight:</b> {D.dogData[G.weight]}                                                   </li>
-            <li className={css.energyLevel}>         <b>Energy Level:</b> {D.dogData[G.energyLevel]}                                              </li>
-            <li className={css.noseColor}>             <b>Nose Color:</b> {D.dogData[G.noseColor]}                                                </li>
-            <li className={css.eyeColor}>               <b>Eye Color:</b> {D.dogData[G.eyeColor]}                                                 </li>
-            <li className={css[G.coat]}>             <b>Coat Color:</b> {D.dogData[G.coat]}                                                </li>
-            <li className={css.personality}>          <b>Personality:</b> {D.dogData[G.personality]}                                              </li>
-          </ul>
-            }
           </div>
-          <div className={css.partnerData}>
-            <div className={css.partnerVisuals}>
-              <h3 className={css.partnerName}>
-                {D.litterData[G.litterBirthday]
-                  ? `Previously matched with ${
-                      D.partnerData[G.adultName]
-                    }.`
-                  : `Currently matched with ${
-                      D.partnerData[G.adultName]
-                    }.`}
-              </h3>
-              <h4 className={css.partnerLastLitter}>
-                {D.litterData[G.litterBirthday]
-                  ? `Last litter born on ${normalizeEpochDate(
-                      D.litterData[
-                        G.litterBirthday
-                      ]!.toLocaleDateString()
-                    ).replace(/.at.*/, "")}`
-                  : `Next litter due on ${normalizeEpochDate(
-                      D.litterData[G.dueDate].toLocaleDateString()
-                    ).replace(/.at.*/, "")}`}
-                {D
-                  .litterData[G.dueDate].toISOString()
-                  .split("T")[0] === new Date().toISOString().split("T")[0]
-                  ? `...That's today!!`
-                  : undefined}
-              </h4>
-              <GroupPhoto
-                id={css[D1T.Group_Photos]}
-                src={D.ids[D1T.Group_Photos]}
-                alt={`${D.dogData[G.adultName]} and ${
-                  D.partnerData[G.adultName]
-                }'s last litter.'`}
-                litterId={D.ids[G.litterId]}
+          <div className={css.partnerPhoto}>
+            <Link
+              href={`/${partnerMorf("sires", "dams")}/${D.ids[G.litterId]}`}
+            >
+              <SmallHeadshot
+                variant={D1T.Headshots_Sm}
+                gender={D.partnerData[G.gender]}
+                src={D.partnerData[G.Headshots_Sm]}
+                alt={D.partnerData[G.adultName]}
+                id={css.Headshots_Sm}
               />
-            </div>
-            <div className={css.partnerPhoto}>
-              <Link
-                href={`/${partnerMorf("sires", "dams")}/${
-                  D.ids[G.litterId]
-                }`}
-              >
-                <SmallHeadshot
-                  variant={D1T.Headshots_Sm}
-                  gender={D.partnerData[G.gender]}
-                  src={D.partnerData[G.Headshots_Sm]}
-                  alt={D.partnerData[G.adultName]}
-                  id={css.Headshots_Sm}
-                />
-              </Link>
-              <h4 className={css.partnerBreeder}>
-                <BreederLine breeder={D.partnerData[G.breeder]} />
-              </h4>
-            </div>
+            </Link>
+            <h4 className={css.partnerBreeder}>
+              <BreederLine breeder={D.partnerData[G.breeder]} />
+            </h4>
           </div>
-        </article>
-      </>
-    );
-  }
+        </div>
+      </article>
+    </>
+  );
+}
