0b8d2aadc840b024d447fd0200734e3347afc741
Ty <noreply@mail.thetyster.dev>
2024-09-17 10:52:37 -0700

    Fix: Adds a default father for families/litters who don't have one.

--- test/migrations/0008_ins_families.sql
+++ test/migrations/0008_ins_families.sql
@@ -1,3 +1,4 @@
 INSERT INTO Families (father, mother, litterId, Group_Photos)
   VALUES
-  (2, 1, 1, "https://media.cherrylanefarmdoodles.com/vz174HUzFuCOWWT9G6qu7yY1Q8wp4ONwlxvqz1Z7jeU5Dad1zl8cBr-Y8W5ASteR0QZy4tZq54DVslw8zCovohphz78o8m2s1i-Z-WUBoOdqje3uorADY-wgPrDNoRUpznaG?iv=Yklg9MLKBatvwl70&k=D5dl2C5W00T8ssDGfWZML8C62881EwvyuTg99jJprA6Gs4veecJsNYNa1W2fV6-dZOLuhf998_SmUbhuE2U0Bnf2Y5tjiXJL0UVlMZhELOSA0wzAgmuI3WJgI8qyG8fFXhYbuzdt_uKp89i0Rn34idQ0wQ2yUtvoOCU6avWH6OcA7VYhieDdfA" );
+  (2, 1, 1, "https://media.cherrylanefarmdoodles.com/vz174HUzFuCOWWT9G6qu7yY1Q8wp4ONwlxvqz1Z7jeU5Dad1zl8cBr-Y8W5ASteR0QZy4tZq54DVslw8zCovohphz78o8m2s1i-Z-WUBoOdqje3uorADY-wgPrDNoRUpznaG?iv=Yklg9MLKBatvwl70&k=D5dl2C5W00T8ssDGfWZML8C62881EwvyuTg99jJprA6Gs4veecJsNYNa1W2fV6-dZOLuhf998_SmUbhuE2U0Bnf2Y5tjiXJL0UVlMZhELOSA0wzAgmuI3WJgI8qyG8fFXhYbuzdt_uKp89i0Rn34idQ0wQ2yUtvoOCU6avWH6OcA7VYhieDdfA"),
+  (4, 1, 2, "/public/images/404couldnt-find-picture.webp");
