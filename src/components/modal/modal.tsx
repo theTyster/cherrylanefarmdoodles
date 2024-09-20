a8344dfd541afdf8a4ff5ac8625814992956390d
Ty <noreply@mail.thetyster.dev>
2024-09-19 17:26:23 -0700

    Fix: Traps focus within modal visible.

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -15,32 +15,67 @@ export default function Modal({
   children: React.ReactNode;
 }): React.JSX.Element {
   const router = useRouter();
-  const modalRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<HTMLDialogElement>(null);
   const modalTl = useRef<gsap.core.Timeline>();
   const svgRef = useRef<SVGSVGElement>(null);
 
   useGSAP(
     () => {
       modalTl.current = gsap.timeline();
-      modalTl.current.to(modalRef.current, {
-        duration: 1,
-        opacity: 1,
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100vh",
-      }).to
-      ('body', { duration: 0, overflow: 'hidden' }, '<')
+      modalTl.current
+        .to(modalRef.current, {
+          duration: css["transitionShort"],
+          opacity: 1,
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100vh",
+        })
+        .to(
+          "body",
+          {
+            duration: 0,
+            overflow: "hidden",
+          },
+          "<"
+        )
+        .to(
+          "div[class^=root-layout]",
+          {
+            duration: 0,
+            filter: "blur(7px)",
+            pointerEvents: "none",
+          },
+          "<"
+        )
+        .call(() => {
+          const container = document.querySelector<HTMLDialogElement>("dialog");
+
+          if (!container)
+            throw new Error(
+              "Dialog element is not found in the dom: " + container
+            );
+
+          //optional: needed only if the container element is not focusable already
+          container.setAttribute("tabindex", "0");
+
+          container.addEventListener("focusout", (ev: FocusEvent) => {
+            if (
+              ev.relatedTarget &&
+              !container.contains(ev.relatedTarget as Node)
+            )
+              container.focus();
+          });
+          container.focus();
+        });
     },
     { dependencies: [modalRef] }
   );
 
   const back = () => modalTl.current!.reverse().then(() => router.back());
-  const handleClick = (
-    event: React.MouseEvent<HTMLDivElement & SVGSVGElement, MouseEvent>
-  ) => {
+  const handlePointer = (event: React.PointerEvent) => {
     const eventClass = (
       event.nativeEvent.target as HTMLElement & SVGSVGElement
     ).attributes.getNamedItem("class");
@@ -53,12 +88,25 @@ export default function Modal({
       }
     }
   };
+
+  const handleKeyboard = (event: React.KeyboardEvent) => {
+    if (event.key === "Escape") {
+      back();
+    }
+  };
+
   return (
     <>
-      <div className={css["container"]} ref={modalRef} onClick={handleClick}>
+      <dialog
+        className={css["container"]}
+        ref={modalRef}
+        onKeyUp={handleKeyboard}
+        onPointerUp={handlePointer}
+        aria-modal="true"
+      >
         <button className={css["exit"]} onClick={back}>
           <svg
-            className={css['x']}
+            className={css["x"]}
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
             width="56"
@@ -85,8 +133,8 @@ export default function Modal({
             />
           </svg>
         </button>
-        <div className={css['child']}>{children}</div>
-      </div>
+        <div className={css["child"]}>{children}</div>
+      </dialog>
     </>
   );
 }
