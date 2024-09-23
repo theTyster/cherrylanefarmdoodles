c5337dc93cc42cd2d5a04a2bdf589885c488144e
Ty <noreply@mail.thetyster.dev>
2024-09-23 12:13:13 -0700

    Fix: modifies wood section parents layout to use currentLitter leade.

--- src/app/@_2wood/(parents)/layout.tsx
+++ src/app/@_2wood/(parents)/layout.tsx
@@ -1,5 +1,5 @@
 export const runtime = "edge";
-import css from "./layout.module.scss";
+import css from "./parents.module.scss";
 
 export default async function WoodSectionParentsLayout({
   children,
@@ -8,7 +8,6 @@ export default async function WoodSectionParentsLayout({
 }): Promise<React.JSX.Element | null> {
   return (
     <>
-      <h2>Current Litter</h2>
       <div className={css.currentLitter}>{children}</div>
     </>
   );
