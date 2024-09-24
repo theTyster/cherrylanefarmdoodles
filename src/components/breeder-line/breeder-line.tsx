e15a7227eed218d1bb0b2bcd4b89c29d5d4bb479
Ty <noreply@mail.thetyster.dev>
2024-09-24 10:22:51 -0700

    Fix: Restores Theme to sass and removes unused variant variable.

--- src/components/breeder-line/breeder-line.tsx
+++ src/components/breeder-line/breeder-line.tsx
@@ -3,36 +3,22 @@ import Image from "next/image";
 import cherry from "@pub/images/cherry.svg";
 import css from "@styles/breeder-line.module.scss";
 
-const variants = {
-  light: "light",
-  dark: "dark",
-} as const;
-
 function BreederLine({
   breeder,
-  variant,
 }: {
   breeder: string;
-  variant?: (typeof variants)[keyof typeof variants];
 }) {
-  if (!variant) variant = variants.light;
   return breeder.match(/Cherry Lane|breeder a/gi) ? (
     <>
-        <span className={css.text}>
-          {breeder + " "}
-          <Image
-            style={
-              variant
-                ? /**Intentionally true. I ended up liking this as the default.*/ {
-                    backgroundColor: css.lightPrimary,
-                  }
-                : undefined
-            }
-            className={css.cherry}
-            src={cherry}
-            alt=""
-          />
-        </span>
+      <span className={css.text}>
+        {breeder + " "}
+        <Image
+          style={{backgroundColor: css.lightPrimary}}
+          className={css.cherry}
+          src={cherry}
+          alt=""
+        />
+      </span>
     </>
   ) : (
     <span className={css.text}>{breeder}</span>
