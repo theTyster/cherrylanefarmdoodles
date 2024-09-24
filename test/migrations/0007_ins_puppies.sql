d052f585da67ec3b7abe9f413b2e6cb4e8fad75f
Ty <noreply@mail.thetyster.dev>
2024-09-24 16:08:38 -0700

    Chore: Updates dog Statuses in D1.

--- test/migrations/0007_ins_puppies.sql
+++ test/migrations/0007_ins_puppies.sql
@@ -1,10 +1,10 @@
 INSERT INTO Puppies (dogId, puppyName, collarColor, availability, litterId)
     VALUES
 /*1*/    (4, "Eloise", "Purple", 'Adopted', 1),
-/*2*/    (5, "Kate", "Yellow", 'Adopted', 1),
+/*2*/    (5, "Kate", "Yellow", 'Held Back', 1),
 /*3*/    (6, "Lady Whistledown", "Pink", 'Adopted', 1),
 /*4*/    (7, "Daphne", "Green", 'Adopted', 1),
-/*5*/    (8, "Francesca", "Nude", 'Available', 1),
+/*5*/    (8, "Francesca", "Nude", 'Adopted', 1),
 /*6*/    (9, "Benedict", "Blue", 'Adopted', 1),
 /*7*/    (10, "Colin", "Red", 'Adopted', 1),
 --09/17 Additions
