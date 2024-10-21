33fea758b9ae8ab642b902ad8f8f8ce545720a6c
Ty <noreply@mail.thetyster.dev>
2024-10-21 06:52:46 -0700

    Feat: Menu is now dynamic based on data in D1.

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,22 +1,63 @@
-// FIX TAB INDEX FOR MENU ITEMS
 "use client";
-// Components
-import Link from "next/link";
-import { font } from "@styles/font";
+export const runtime = "edge";
+
+//Utilities
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 // Styles
 import navCSS from "@styles/nav.module.scss";
 
-export const runtime = "edge";
+// Components
+import Link from "next/link";
 
-function Nav() {
+export const SUBMENU_STATES = {
+  Initial: "Initial",
+  Mothers: "Mothers",
+  Litters: "Litters",
+} as const;
+
+export const MENU_STATES = {
+  open: "open",
+  closed: "closed",
+  submenu: "submenu",
+} as const;
+
+export type MenuStateTypes = (typeof MENU_STATES)[keyof typeof MENU_STATES];
+export type SubMenuStateTypes =
+  (typeof SUBMENU_STATES)[keyof typeof SUBMENU_STATES];
+
+function NavMenu({
+  menuData,
+}: {
+  menuData: {
+    [SUBMENU_STATES.Mothers]: React.ReactNode;
+    [SUBMENU_STATES.Litters]: React.ReactNode;
+  };
+}) {
+  const [subMenuState, setSubMenuState]: [
+    SubMenuStateTypes,
+    React.Dispatch<React.SetStateAction<SubMenuStateTypes>>
+  ] = useState(SUBMENU_STATES["Initial"] as SubMenuStateTypes);
+
+  // After the initial load the menu state is switched to closed. 'open' is the
+  // correct initialization.
+  const [menuState, setMenuState]: [
+    MenuStateTypes,
+    React.Dispatch<React.SetStateAction<MenuStateTypes>>
+  ] = useState(MENU_STATES["open"] as MenuStateTypes);
+
   const menuTween: gsap.TweenVars = {
     display: "flex",
     width: "100%",
+    height: "100%",
+    padding: "0.25em 0",
     alignItems: "center",
+    overflow:
+      () => window.innerWidth < Number.parseFloat(navCSS["LgPhoneViewport"])
+        ? "scroll"
+        : "visible",
     position: "fixed",
     top: 0,
     left: 0,
@@ -27,29 +68,22 @@ function Nav() {
   const buttonTween: gsap.TweenVars = {
     position: "absolute",
     bottom: 5,
-    height: Number.parseFloat(navCSS["menuButtonHeight"]) / 2 + "px",
-    borderRadius: Number.parseFloat(navCSS["buttonRadius"]) - 0.5 + "rem",
     boxShadow: "transparent 0px 0px 0px 0px",
-    fontSize: "1rem",
     backgroundColor: navCSS.lightTertiaryCherry,
     color: navCSS.darkTertiaryCherry,
-    display: "none", // use this until their is more to put in the menu.
-    opacity: 0, // use this until their is more to put in the menu.
+    display: "none",
+    opacity: 0,
   };
 
   const menuRef = useRef<HTMLElement>(null),
+    exitHandler = useRef<() => void>(),
     menuTl = useRef<gsap.core.Timeline>(),
-    buttonRef = useRef<HTMLButtonElement>(null),
-    refChecker = () => {
-      const button = buttonRef.current;
-      const tl = menuTl.current;
-      const nav = menuRef.current;
+    mothersMenuRef = useRef<HTMLLIElement>(null),
+    littersMenuRef = useRef<HTMLLIElement>(null),
+    backToMainMenuRef = useRef<HTMLLIElement>(null),
+    buttonRef = useRef<HTMLButtonElement>(null);
 
-      if (!tl) throw new Error("Gsap timeline not found in the menu: " + tl);
-      if (!nav) throw new Error("Nav not found in the dom: " + nav);
-      if (!button) throw new Error("Button not found: " + button);
-      return { button, tl, nav };
-    };
+  /**Protects focus while the menu is closed.*/
   function setMenuInerts(addOrRemove: "add" | "remove") {
     const noInertLinks = () =>
       menuRef.current?.querySelectorAll("a").forEach((link) => {
@@ -81,134 +115,188 @@ function Nav() {
     }
   }
 
-  useGSAP(
-    () => {
-      menuTl.current = gsap.timeline({
-        paused: true,
-        defaults: { duration: Number.parseFloat(navCSS["transitionShort"]) },
-      });
+  const menuHeight: number = Number.parseFloat(navCSS["menuHeight"]);
 
-      gsap
-        .to(`#${navCSS["title-menu-button"]}`, {
-          boxShadow: `${navCSS.darkTertiaryCherry} ${navCSS.boxShadowX}px ${navCSS.boxShadowY}px ${navCSS.boxShadowBlur}px`,
-        })
-        .play();
-
-      setMenuInerts("add");
-      menuTl.current
-        .call(setMenuInerts, ["remove"])
-        .to(menuRef.current, { width: "100%", duration: 0 })
-        .to(`#${navCSS["title-menu-button"]}`, {
-          transition: "none",
-          duration: 0,
-        })
-        .to(`.${navCSS["menu"]}`, menuTween, "<")
-        .to(`#${navCSS["title-menu-button"]}`, buttonTween, "<")
-        .to(`#${navCSS["title-menu-button"]}`, {
-          clearProps: "transition",
-          duration: 0,
-        })
-        .to(`.${navCSS["menu"]}`, { flexWrap: "wrap", duration: 0 }, ">");
-      const { button } = refChecker();
-      button.addEventListener("click", firstClickHandler, {
-        once: true,
-        passive: true,
+  const setMenuPosition = async (height: number) => {
+    console.log("height", height);
+    if (window.innerWidth < Number.parseFloat(navCSS["LgPhoneViewport"]))
+      await gsap.to(`.${navCSS["nav"]}`, {
+        bottom: 0,
+        height,
+        duration: navCSS["transitionShort"],
       });
-    },
-    {
-      scope: menuRef,
-      dependencies: [buttonRef, menuTween, buttonTween],
-    }
-  );
+  };
 
   const reverseTl = async () => {
-    const { tl, button } = refChecker();
-    await tl.reversed(!tl.reversed());
-    button.focus(); // This prevents menu items from stealing focus after the menu closes.
+    if (!menuTl.current) throw new Error("Timeline not found.");
+    if (!buttonRef.current) throw new Error("Button not found.");
+    setMenuPosition(menuHeight);
+    await menuTl.current.reversed(!menuTl.current.reversed());
+    buttonRef.current.focus(); // This prevents menu items from stealing focus after the menu closes.
   };
 
-  const playTl = async () => {
-    const { tl } = refChecker();
-    await tl.play(0);
+  const buttonEventHandler = async () => {
+    await reverseTl();
+    exitHandler.current!();
   };
 
-  const tlIsOpen = () => {
-    const { tl } = refChecker();
-    if (tl.progress() === 1) return true;
-    else if (tl.progress() === 0) return false;
-    else
-      throw new Error("Timeline is not at the start or end: " + tl.progress());
-  };
-
-  const keyCloser = (e: KeyboardEvent) => {
-    if (e.key === "Escape" || e.key === "Enter" || e.key === " ") {
-      subsequentClicksHandler();
+  const handleExit = async (e: MouseEvent | KeyboardEvent | Event) => {
+    if (
+      ((e as KeyboardEvent).key === "Escape" ||
+        (e as KeyboardEvent).key === "Enter" ||
+        (e as KeyboardEvent).key === " " ||
+        e.type === "scroll" ||
+        (e.type === "click" &&
+          !menuRef.current?.contains(e.target as HTMLElement) &&
+          (backToMainMenuRef.current !== e.target ||
+            mothersMenuRef.current !== e.target ||
+            littersMenuRef.current !== e.target ||
+            menuRef.current !== e.target))) &&
+      (menuState === MENU_STATES["open"] || MENU_STATES["submenu"])
+    ) {
+      window.removeEventListener("click", handleExit);
+      window.removeEventListener("keyup", handleExit);
+      window.removeEventListener("scroll", handleExit);
+      if (menuState === "submenu") {
+        setMenuState(MENU_STATES["open"]);
+        await setMenuPosition(menuHeight);
+      }
+      setSubMenuState(SUBMENU_STATES["Initial"]);
+      await reverseTl();
+    } else if (
+      (e.type === "click" || e.type === "scroll") &&
+      mothersMenuRef.current === e.target
+    ) {
+      setMenuState(MENU_STATES["submenu"]);
+      setMenuPosition(350);
+      setSubMenuState(SUBMENU_STATES["Mothers"]);
+    } else if (e.type === "click" && littersMenuRef.current === e.target) {
+      setMenuState(MENU_STATES["submenu"]);
+      setMenuPosition(350);
+      setSubMenuState(SUBMENU_STATES["Litters"]);
+    } else if (
+      (e.type === "click" || e.type === "scroll") &&
+      backToMainMenuRef.current === e.target
+    ) {
+      setMenuState(MENU_STATES["open"]);
+      setMenuPosition(menuHeight);
+      setSubMenuState(SUBMENU_STATES["Initial"]);
     }
   };
 
-  const addCloserListeners = () => {
-    document.addEventListener("click", subsequentClicksHandler);
-    document.addEventListener("keyup", keyCloser);
-    document.addEventListener("scroll", subsequentClicksHandler);
-  };
+  useGSAP(() => {
+    setMenuInerts("add");
+    gsap
+      .to(`#${navCSS["title-menu-button"]}`, {
+        boxShadow: `${navCSS.darkTertiaryCherry} ${navCSS.boxShadowX}px ${navCSS.boxShadowY}px ${navCSS.boxShadowBlur}px`,
+      })
+      .play();
 
-  const removeCloserListeners = () => {
-    document.removeEventListener("click", subsequentClicksHandler);
-    document.removeEventListener("keyup", keyCloser);
-    document.removeEventListener("scroll", subsequentClicksHandler);
-  };
+    menuTl.current = gsap.timeline({
+      defaults: { duration: Number.parseFloat(navCSS["transitionShort"]) },
+    });
 
-  const subsequentClicksHandler = async () => {
-    const { button } = refChecker();
-    removeCloserListeners();
-    await reverseTl();
+    menuTl.current
+      .to(menuRef.current, { width: "100%", duration: 0 })
+      .to(`#${navCSS["title-menu-button"]}`, {
+        transition: "none",
+        duration: 0,
+      })
+      .to(`.${navCSS["menu"]}`, menuTween, "<")
+      .to(`#${navCSS["title-menu-button"]}`, buttonTween, "<")
+      .to(`#${navCSS["title-menu-button"]}`, {
+        clearProps: "transition",
+        duration: 0,
+      })
+      .to(`.${navCSS["menu"]}`, { flexWrap: "wrap", duration: 0 }, ">")
+      .call(setMenuInerts, ["remove"])
+      .call(setMenuState, ["Initial"])
+      .call(() => setMenuState(menuState === "closed" ? "open" : "closed"));
+    menuTl.current.reversed(true);
 
-    if (tlIsOpen()) {
-      addCloserListeners();
-      setMenuInerts("remove");
-    } else {
-      button.addEventListener("click", subsequentClicksHandler, {
-        once: true,
+    exitHandler.current = () => {
+      window.addEventListener("click", handleExit, {
         passive: true,
       });
-      setMenuInerts("add");
-    }
-  };
+      window.addEventListener("keyup", handleExit, {
+        passive: true,
+      });
+      window.addEventListener("scroll", handleExit, {
+        passive: true,
+      });
+    };
 
-  const firstClickHandler = async () => {
-    await playTl();
-    addCloserListeners();
-    setMenuInerts("remove");
-  };
+    return () => {
+      window.removeEventListener("click", handleExit);
+      window.removeEventListener("keyup", handleExit);
+      window.removeEventListener("scroll", handleExit);
+    };
+  });
 
   return (
-    <>
-      <nav className={navCSS["sliderMenu"]} ref={menuRef}>
-        <button
-          className={font.className}
-          id={navCSS["title-menu-button"]}
-          ref={buttonRef}
-          tabIndex={0}
-        >
-          Menu
-        </button>
-        <menu className={navCSS["menu"]}>
-          <Link className={navCSS["link"]} href="/">
-            Home
-          </Link>
-          <Link className={navCSS["link"]} href="/about">
-            About
-          </Link>
-          <Link className={navCSS["link"]} href="/dams/1">
-            Mothers
-          </Link>
-          <Link className={navCSS["link"]} href="/litter/1">
-            Litters
-          </Link>
-        </menu>
-      </nav>
-    </>
+    <nav className={navCSS["nav"]} ref={menuRef}>
+      <button
+        onClick={buttonEventHandler}
+        id={navCSS["title-menu-button"]}
+        ref={buttonRef}
+        tabIndex={0}
+      >
+        Menu
+      </button>
+      <menu className={navCSS["menu"]}>
+        {subMenuState === SUBMENU_STATES.Initial ? (
+          <>
+            <Link href="/" className={navCSS["menu-item"]}>
+              Home
+            </Link>
+            <Link href="/about" className={navCSS["menu-item"]}>
+              About
+            </Link>
+          </>
+        ) : null}
+        {(() => {
+          if (subMenuState === SUBMENU_STATES.Mothers)
+            return (
+              <div className={navCSS["submenu"]}>
+                <li
+                  className={`${navCSS["menu-item"]} ${navCSS["back"]}`}
+                  ref={backToMainMenuRef}
+                >
+                  &lt; Back
+                </li>
+                {menuData[SUBMENU_STATES.Mothers]}
+              </div>
+            );
+          if (subMenuState === SUBMENU_STATES.Initial)
+            return (
+              <li className={`${navCSS["menu-item"]}`} ref={mothersMenuRef}>
+                Mothers
+              </li>
+            );
+        })()}
+        {(() => {
+          if (subMenuState === SUBMENU_STATES.Litters)
+            return (
+              <div className={`${navCSS["submenu"]} ${navCSS["litter-submenu"]}`}>
+                <li
+                  className={`${navCSS["menu-item"]} ${navCSS["back"]}`}
+                  ref={backToMainMenuRef}
+                >
+                  &lt; Back
+                </li>
+                {menuData[SUBMENU_STATES.Litters]}
+              </div>
+            );
+          if (subMenuState === SUBMENU_STATES.Initial)
+            return (
+              <li className={navCSS["menu-item"]} ref={littersMenuRef}>
+                Recent Litters
+              </li>
+            );
+        })()}
+      </menu>
+    </nav>
   );
 }
 
-export default Nav;
+export default NavMenu;
