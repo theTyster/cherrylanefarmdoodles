152792a0fdd8e13b46bd8676c90866e63a7bbea8
Ty <noreply@mail.thetyster.dev>
2024-10-21 17:01:01 -0700

    Fix: Input focus being shifted around during animation.

--- src/components/subscribe-buttons/puppifications.tsx
+++ src/components/subscribe-buttons/puppifications.tsx
@@ -1,21 +1,28 @@
 "use client";
 export const runtime = "edge";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import css from "@styles/puppifications.module.scss";
 import Image from "next/image";
+import { sleep } from "thetyster-utils";
 
 function Puppifications() {
-  // State to track if the form is visible
   const [formVisible, setFormVisible] = useState(false);
   const [subscription, setSubscription] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const submitRef = useRef<HTMLButtonElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  // Function to handle the form submission
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const input = form[0] as HTMLInputElement;
-    const email = input.value;
-    if (!email) return; // No action if no email.
+    const input = inputRef.current;
+    const email = input ? input.value : "";
+    if (!email)
+      return input
+        ? input.focus()
+        : console.warn(
+            "No email address provided in Puppification subscription."
+          ); 
     fetch("/api/subscribe", {
       method: "POST",
       headers: {
@@ -28,66 +35,109 @@ function Puppifications() {
     animateSub();
   };
 
-  // Function to handle the button click
-  const animateSub = () => {
+  const animateSub = async () => {
+    const input = inputRef.current;
+    const form = formRef.current;
+    const button = buttonRef.current;
     setFormVisible(!formVisible);
+    await sleep(0.2);
+    if (!formVisible && form && button && input) {
+      form.inert = false;
+      input.focus();
+      button.inert = true;
+    }
+    if (formVisible && button && form) {
+      button.inert = false;
+      button.focus();
+      form.inert = true;
+    }
   };
 
-  const handleKeyup = (e: React.KeyboardEvent) => {
-    if (e.key === "Escape") {
-      animateSub();
-    }
-    if (e.key === "Enter") {
-      animateSub();
-    }
-  };
-
-  // Return the button and form
   return (
     <>
       <div className={css["container"]}>
-        <div style={{height: formVisible? '100%': 0 }} className={css["iris"]}>
-            <span className={css["top-line"]}>Enter your email</span>
-            <Image
-              className={css["envelope-doodle"]}
-              src={"/images/envelope-doodle.png"}
-              width={130}
-              height={130}
-              alt={"Puppy holding an envelope."}
-            />
-            <form className={css["form"]} onSubmit={handleSubmit}>
-              <input
-                onKeyUp={handleKeyup}
-                type="email"
-                placeholder="Email Address"
-              />
-              <button className={css["button"]} type="submit">
+        <div
+          style={{ height: formVisible ? "100%" : 0 }}
+          className={css["iris"]}
+        >
+          <span className={css["top-line"]}>Enter your email</span>
+          <Image
+            className={css["envelope-doodle"]}
+            src={"/images/envelope-doodle.png"}
+            width={130}
+            height={130}
+            alt={"Puppy holding an envelope."}
+          />
+          <form
+            ref={formRef}
+            inert={
+              formVisible
+                ? (undefined as unknown as boolean)
+                : ("true" as unknown as boolean)
+            }
+            className={css["form"]}
+            onSubmit={handleSubmit}
+          >
+            <>
+              {formVisible ? (
+                <input
+                  ref={inputRef}
+                  type="email"
+                  placeholder="Email Address"
+                  onBlur={(e) => {
+                    if (e.relatedTarget === submitRef.current) {
+                      handleSubmit(e);
+                    }
+                    else {
+                      animateSub();
+                    }
+                  }}
+                />
+              ) : (
+                <input
+                  /*inert <-- Handled by animateSub()*/
+                  ref={inputRef}
+                  type="email"
+                  placeholder="Email Address"
+                />
+              )}
+              <button ref={submitRef} className={css["button"]} type="submit">
                 Subscribe
               </button>
-            </form>
-          </div>
-        <div style={{height: formVisible? 0 : '100%'}}className={css["iris"]}>
-            <span className={css["top-line"]}>
-              {subscription
-                ? "Check your inbox for a confirmation email"
-                : "Be the first to know when more puppies arrive"}
-            </span>
-            <style jsx>{`
-              .${css["button"]}:hover {
-                ${subscription ? "transform: scale(1);" : ""}
-                ${subscription ? "cursor: auto;" : ""}
-              }
-            `}</style>
-            <button
-              style={subscription ? { boxShadow: "none" } : undefined}
-              className={`${css["button"]} ${css["puppifications"]} ${css["woodgrain-light"]}`}
-              onClick={subscription ? () => undefined : animateSub}
-            >
-              {`${
-                subscription ? "Pending confirmation" : "Enable Puppifications"
-              }`}
-            </button>
-          </div>
+            </>
+          </form>
+        </div>
+        <div
+          style={{ height: formVisible ? 0 : "100%" }}
+          className={css["iris"]}
+        >
+          <span className={css["top-line"]}>
+            {subscription
+              ? "Check your inbox for a confirmation email"
+              : "Be the first to know when more puppies arrive"}
+          </span>
+          <style jsx>{`
+            .${css["button"]}:hover {
+              ${subscription ? "transform: scale(1);" : ""}
+              ${subscription ? "cursor: auto;" : ""}
+            }
+          `}</style>
+          <button
+            inert={
+              subscription
+                ? ("true" as unknown as boolean)
+                : (undefined as unknown as boolean)
+            }
+            style={subscription ? { boxShadow: "none" } : undefined}
+            className={`${css["button"]} ${css["puppifications"]} ${css["woodgrain-light"]}`}
+            onClick={subscription ? () => undefined : animateSub}
+            ref={buttonRef}
+          >
+            {`${
+              subscription ? "Pending confirmation" : "Enable Puppifications"
+            }`}
+          </button>
+        </div>
       </div>
     </>
   );
