6984c67041389e8daa22bf98f7f5c2dd94921368
Ty <noreply@mail.thetyster.dev>
2024-10-22 06:36:01 -0700

    Type: Adds type for data passed into PuppiesLeft component.

---
+++ src/types/puppies-left.ts
@@ -0,0 +1,7 @@
+import { GlobalNameSpaces  as G } from "@/constants/data";
+
+export type PuppiesLeftData = {
+  [G.availablePuppies]: number;
+  [G.applicantsInQueue]: number;
+  [G.totalPuppies]: number;
+}
