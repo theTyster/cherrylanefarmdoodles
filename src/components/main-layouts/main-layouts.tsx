5f838c5ae9643895759f8f644c4c64930554093c
Ty <noreply@mail.thetyster.dev>
2024-10-25 10:45:46 -0700

    Fix: Move Layout slots to a client component.
    Explanation: This was done to make handling events in the menu easier.
    We can pass a ref down to the menu event handlers instead of trying to
    filter out everything in the dom.

---
+++ src/components/main-layouts/main-layouts.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useRef } from "react";
+import Nav from "@/components/nav/menu-items";
+import { type MenuDataType } from "@/constants/nav";
+
+function MainLayouts({
+  menuData,
+  modal,
+  children,
+  _2wood,
+  _3tan,
+  css,
+}: {
+  menuData: MenuDataType;
+  modal: React.ReactNode;
+  children: React.ReactNode;
+  _2wood: React.ReactNode;
+  _3tan: React.ReactNode;
+  css: Record<string, string>;
+}) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  return (
+    <>
+      <Nav menuData={menuData} exitRef={contentRef} />
+      <div ref={contentRef}>
+        {modal}
+        <div className={css["white-layout"]}>
+          <div className={css["left-flex"]} />
+          <div className={css["content-box"]}>{children}</div>
+          <div className={css["right-flex"]} />
+        </div>
+        <div className={`${css["wood-layout"]}`}>
+          <div className={css["left-flex"]} />
+          <div className={css["content-box"]}>{_2wood}</div>
+          <div className={css["right-flex"]} />
+        </div>
+        <div className={css["tan-layout"]}>
+          <div className={css["left-flex"]} />
+          <div className={css["content-box"]}>{_3tan}</div>
+          <div className={css["right-flex"]} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainLayouts;
