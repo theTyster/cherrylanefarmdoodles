42f55d124ebb0959aec4b911cf8d91c2a12f4354
Ty <noreply@mail.thetyster.dev>
2024-10-28 15:17:59 -0700

    Fix: Sets 'mothers' menu items to the 'dams/' route instead of 'litter/'.

--- src/components/nav/menu-items.tsx
+++ src/components/nav/menu-items.tsx
@@ -6,9 +6,7 @@ import NavMenu from "@/components/nav/nav";
 import navCSS from "@styles/nav.module.scss";
 
 // Types
-import { MenuItemData, MenuDataType
-} from "@/constants/nav";
-
+import { MenuItemData, MenuDataType } from "@/constants/nav";
 
 export const runtime = "edge";
 
@@ -25,27 +23,28 @@ function SubMenu({
   }: { motherData: MenuItemData; litterData: MenuItemData } = menuData;
 
   function renderSubMenuOf(
-    menuItemCollection: typeof motherData | typeof litterData
+    menuItemCollection: typeof motherData | typeof litterData,
+    linkPrefix: string
   ) {
     return menuItemCollection.map((item) => (
       <Link
         key={item.name + item.id}
-        href={`/litter/${item.id}`}
+        href={`/${linkPrefix}/${item.id}`}
         className={navCSS["submenu-item"]}
       >
         {item.name}
       </Link>
     ));
   }
-    return (
-      <NavMenu
-        menuData={{
-          Mothers: <>{renderSubMenuOf(motherData)}</>,
-          Litters: <>{renderSubMenuOf(litterData)}</>,
-        }}
-        exitRef={exitRef}
-      />
-    );
+  return (
+    <NavMenu
+      menuData={{
+        Mothers: <>{renderSubMenuOf(motherData, "dams")}</>,
+        Litters: <>{renderSubMenuOf(litterData, "litter")}</>,
+      }}
+      exitRef={exitRef}
+    />
+  );
 }
 
 export default SubMenu;
