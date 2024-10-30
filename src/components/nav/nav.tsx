a274c3c3513a7550fcee899f708d5da0368f6cfc
Ty <noreply@mail.thetyster.dev>
2024-10-30 15:54:50 -0700

    Feat: Adds memoization to Nav for better performance.

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -3,7 +3,7 @@ export const runtime = "edge";
 
 //Utilities
 import gsap from "gsap";
-import { useRef, useState } from "react";
+import { useRef, useState, useMemo } from "react";
 
 // Styles
 import navCSS from "@styles/nav.module.scss";
@@ -55,204 +55,257 @@ function NavMenu({
     backToMainMenuRef = useRef<HTMLButtonElement>(null),
     buttonRef = useRef<HTMLButtonElement>(null);
 
-  class MenuAnimation {
-    setMenuState: React.Dispatch<React.SetStateAction<MenuStateTypes>>;
-    menuState: MenuStateTypes;
-    buttonTween?: gsap.core.Tween;
+  const MenuAnimation = useMemo(
+    () =>
+      class MenuAnimation {
+        setMenuState: React.Dispatch<React.SetStateAction<MenuStateTypes>>;
+        menuState: MenuStateTypes;
+        subMenuState: SubMenuStateTypes;
+        setSubMenuState: React.Dispatch<
+          React.SetStateAction<SubMenuStateTypes>
+        >;
+        exit: React.RefObject<HTMLDivElement>;
+        buttonTween?: gsap.core.Tween;
 
-    constructor(
-      setMenuState: React.Dispatch<React.SetStateAction<MenuStateTypes>>,
-      menuState: MenuStateTypes
-    ) {
-      this.setMenuState = setMenuState;
-      this.menuState = menuState;
-    }
+        constructor(
+          setMenuState: React.Dispatch<React.SetStateAction<MenuStateTypes>>,
+          menuState: MenuStateTypes,
+          subMenuState: SubMenuStateTypes,
+          setSubMenuState: React.Dispatch<
+            React.SetStateAction<SubMenuStateTypes>
+          >,
+          exit: React.RefObject<HTMLDivElement>
+        ) {
+          this.setMenuState = setMenuState;
+          this.menuState = menuState;
+          this.setSubMenuState = setSubMenuState;
+          this.subMenuState = subMenuState;
+          this.exit = exit;
+        }
 
-    menuOpenTween: gsap.TweenVars = {
-      display: "flex",
-      width: "100%",
-      height: "100%",
-      padding: "0.25em 0",
-      alignItems: "center",
-      position: "fixed",
-      top: 0,
-      left: 0,
-      ease: "linear",
-      delay: 0,
-    };
-
-    menuCloseTween: gsap.TweenVars = {
-      width: 0,
-    };
-
-    buttonOpenTween: gsap.TweenVars = { y: 50, opacity: 0 };
-    buttonCloseTween: gsap.TweenVars = { y: 0, opacity: 1 };
-
-    menuNavigationButtons = [mothersMenuRef, littersMenuRef, backToMainMenuRef];
-
-    menuHeight = Number.parseFloat(navCSS["menuHeight"]);
-
-    // Tweens {
-    tweenMenuButton(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
-      return gsap[toOrFrom](`#${navCSS["title-menu-button"]}`, tween);
-    }
-
-    tweenMenu(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
-      return gsap[toOrFrom](`.${navCSS["menu"]}`, tween);
-    }
-
-    tweenNav(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
-      return gsap[toOrFrom](`.${navCSS["nav"]}`, tween);
-    }
-
-    async menuOpen() {
-      console.log("opening");
-      if (this.menuState !== MENU_STATES["closed"]) return;
-      this.menuStateSetter(MENU_STATES["superPositioned"]);
-      menuRef.current!.style.display = "flex";
-      menuRef.current!.style.overflow = "hidden";
-
-      this.tweenMenuButton(this.buttonOpenTween).then(
-        () => (buttonRef.current!.style.display = "none")
-      );
-      await this.tweenNav({
-        width: "100%",
-      });
-
-      menuRef.current!.style.overflow = "";
-      this.menuStateSetter(MENU_STATES["open"]);
-      this.addListeners();
-      await this.tweenMenu(this.menuOpenTween);
-    }
-
-    async menuClose() {
-      console.log("closing");
-      if (this.menuState !== MENU_STATES["open"]) return;
-      this.removeListeners();
-      this.menuStateSetter(MENU_STATES["superPositioned"]);
-      menuRef.current!.style.overflow = "hidden";
-      buttonRef.current!.style.display = "block";
-
-      this.tweenMenuButton(this.buttonCloseTween);
-      await this.tweenNav({
-        width: 0,
-      }).then(() => (menuRef.current!.style.display = "none"));
-
-      menuRef.current!.style.overflow = "";
-      this.menuStateSetter(MENU_STATES["closed"]);
-      //await this.tweenMenu(this.menuOpenTween, "from");
-    }
-    async positionMenuForScreenSize(height: number) {
-      if (window.innerWidth >= Number.parseFloat(navCSS["LgPhoneViewport"]))
-        await this.tweenNav({
+        menuOpenTween: gsap.TweenVars = {
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          padding: "0.25em 0",
+          alignItems: "center",
+          position: "fixed",
           top: 0,
-          height,
-          duration: navCSS["transitionLong"],
-        });
-      else window.innerWidth < Number.parseFloat(navCSS["LgPhoneViewport"]);
-      await this.tweenNav({
-        bottom: 0,
-        height,
-        duration: navCSS["transitionShort"],
-      });
-    }
-    // }
+          left: 0,
+          ease: "linear",
+          delay: 0,
+        };
 
-    exitEventHandler() {
-      this.menuClose();
-    }
-    mouseExitEventHandlerFunc(/*e: MouseEvent*/) {
-      this.menuClose();
-    }
-    keyboardExitEventHandlerFunc(e: KeyboardEvent) {
-      if (
-        (e.key === "Escape" || e.key === "Backspace") &&
-        this.menuState === "open"
-      )
-        this.menuClose();
-      else if (
-        (e.key === "Enter" || e.key === "Space") &&
-        this.menuState === "closed"
-      )
-        this.menuOpen();
-      else return false;
-    }
-    scrollExitEventHandlerFunc(/*e: Event*/) {
-      this.menuClose();
-    }
+        menuCloseTween: gsap.TweenVars = {
+          width: 0,
+        };
 
-    /**
-     * This handler is intentionally leaving the submenu items alone.
-     * This provides a better UX since users in a specific submenu are
-     * likely to want to view multiple items in that category without needing
-     * to click through to it each time.
-     **/
-    mouseMenuEventHandlerFunc(e: React.MouseEvent<HTMLElement, MouseEvent>) {
-      if (
-        (e.target as HTMLElement).classList.contains(navCSS["menu-item"]) &&
-        !this.menuNavigationButtons.some(
-          (button) => e.target === button.current
-        )
-      )
-        this.menuClose();
-    }
+        buttonOpenTween: gsap.TweenVars = { y: 50, opacity: 0 };
+        buttonCloseTween: gsap.TweenVars = { y: 0, opacity: 1 };
 
-    removeListeners() {
-      exitRef.current!.removeEventListener(
-        "click",
-        this.exitEventHandler.bind(this)
-      );
-      window.removeEventListener("keyup", this.exitEventHandler.bind(this));
-      window.removeEventListener("scroll", this.exitEventHandler.bind(this));
-    }
-    addListeners() {
-      exitRef.current!.addEventListener(
-        "click",
-        this.mouseExitEventHandlerFunc.bind(this),
-        {
-          once: true,
-          passive: true,
+        menuNavigationButtons = [
+          mothersMenuRef,
+          littersMenuRef,
+          backToMainMenuRef,
+        ];
+
+        menuHeight = Number.parseFloat(navCSS["menuHeight"]);
+
+        // Tweens {
+        tweenMenuButton(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
+          return gsap[toOrFrom](`#${navCSS["title-menu-button"]}`, tween);
         }
-      );
-      window.addEventListener(
-        "keyup",
-        this.keyboardExitEventHandlerFunc.bind(this),
-        {
-          passive: true,
-        }
-      );
-      window.addEventListener(
-        "scroll",
-        this.scrollExitEventHandlerFunc.bind(this),
-        {
-          once: true,
-          passive: true,
-        }
-      );
-    }
 
-    menuStateSetter(menuState: MenuStateTypes) {
-      this.menuState = menuState;
-      this.setMenuState(this.menuState);
-    }
-    async toggleMenu(menuState: MenuStateTypes) {
-      if (!buttonRef.current) throw new Error("Button not found.");
+        tweenMenu(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
+          return gsap[toOrFrom](`.${navCSS["menu"]}`, tween);
+        }
 
-      if (menuState === MENU_STATES["open"]) {
-        await this.menuClose();
-      } else if (menuState === MENU_STATES["closed"]) {
-        await this.menuOpen();
-      }
-    }
-  } /** END MENUANIMATION CLASS*/
+        tweenNav(tween: gsap.TweenVars, toOrFrom: "to" | "from" = "to") {
+          return gsap[toOrFrom](`.${navCSS["nav"]}`, tween);
+        }
+
+        async menuOpen() {
+          if (this.menuState !== MENU_STATES["closed"]) return;
+          this.menuStateSetter(MENU_STATES["superPositioned"]);
+          menuRef.current!.style.display = "flex";
+          menuRef.current!.style.overflow = "hidden";
+
+          this.tweenMenuButton(this.buttonOpenTween).then(
+            () => (buttonRef.current!.style.display = "none")
+          );
+          await this.tweenNav({
+            width: "100%",
+          });
+
+          menuRef.current!.style.overflow = "";
+          this.menuStateSetter(MENU_STATES["open"]);
+          this.addListeners();
+          await this.tweenMenu(this.menuOpenTween);
+        }
+
+        async menuClose() {
+          if (this.menuState !== MENU_STATES["open"]) return;
+          this.removeListeners();
+          this.menuStateSetter(MENU_STATES["superPositioned"]);
+          menuRef.current!.style.overflow = "hidden";
+          buttonRef.current!.style.display = "block";
+
+          this.tweenMenuButton(this.buttonCloseTween);
+          await this.tweenNav({
+            width: 0,
+          }).then(() => (menuRef.current!.style.display = "none"));
+
+          menuRef.current!.style.overflow = "";
+          this.menuStateSetter(MENU_STATES["closed"]);
+          this.subMenuStateSetter(SUBMENU_STATES["Initial"]);
+        }
+
+        async positionMenuForScreenSize(height: number) {
+          if (window.innerWidth >= Number.parseFloat(navCSS["LgPhoneViewport"]))
+            await this.tweenNav({
+              top: 0,
+              height,
+              duration: navCSS["transitionLong"],
+            });
+          else window.innerWidth < Number.parseFloat(navCSS["LgPhoneViewport"]);
+          await this.tweenNav({
+            bottom: 0,
+            height,
+            duration: navCSS["transitionShort"],
+          });
+        }
+        // }
+
+        exitEventHandler() {
+          this.menuClose();
+        }
+        mouseExitEventHandlerFunc(/*e: MouseEvent*/) {
+          this.menuClose();
+        }
+        keyboardExitEventHandlerFunc(e: KeyboardEvent) {
+          if (
+            (e.key === "Escape" || e.key === "Backspace") &&
+            this.menuState === "open"
+          )
+            this.menuClose();
+          else if (
+            (e.key === "Enter" || e.key === "Space") &&
+            this.menuState === "closed"
+          )
+            this.menuOpen();
+          else return false;
+        }
+        scrollExitEventHandlerFunc(/*e: Event*/) {
+          this.menuClose();
+        }
+
+        /**
+         * This handler is intentionally leaving the submenu items alone.
+         * This provides a better UX since users in a specific submenu are
+         * likely to want to view multiple items in that category without needing
+         * to click through to it each time.
+         **/
+        mouseMenuEventHandlerFunc(
+          e: React.MouseEvent<HTMLElement, MouseEvent>
+        ) {
+          if (
+            ((e.target as HTMLElement).classList.contains(
+              navCSS["menu-item"]
+            ) ||
+              (e.target as HTMLElement).classList.contains(
+                navCSS["submenu-item"]
+              )) &&
+            !this.menuNavigationButtons.some(
+              (button) => e.target === button.current
+            )
+          )
+            this.menuClose();
+        }
+
+        removeListeners() {
+          this.exit.current!.removeEventListener(
+            "click",
+            this.exitEventHandler.bind(this)
+          );
+          window.removeEventListener("keyup", this.exitEventHandler.bind(this));
+          window.removeEventListener(
+            "scroll",
+            this.exitEventHandler.bind(this)
+          );
+        }
+        addListeners() {
+          this.exit.current!.addEventListener(
+            "click",
+            this.mouseExitEventHandlerFunc.bind(this),
+            {
+              once: true,
+              passive: true,
+            }
+          );
+          window.addEventListener(
+            "keyup",
+            this.keyboardExitEventHandlerFunc.bind(this),
+            {
+              passive: true,
+            }
+          );
+          window.addEventListener(
+            "scroll",
+            this.scrollExitEventHandlerFunc.bind(this),
+            {
+              once: true,
+              passive: true,
+            }
+          );
+        }
+
+        subMenuStateSetter(subMenuState: SubMenuStateTypes) {
+          this.subMenuState = subMenuState;
+          this.setSubMenuState(this.subMenuState);
+        }
+
+        menuStateSetter(menuState: MenuStateTypes) {
+          this.menuState = menuState;
+          this.setMenuState(this.menuState);
+        }
+        async toggleMenu(menuState: MenuStateTypes) {
+          if (!buttonRef.current) throw new Error("Button not found.");
+
+          if (menuState === MENU_STATES["open"]) {
+            await this.menuClose();
+          } else if (menuState === MENU_STATES["closed"]) {
+            await this.menuOpen();
+          }
+        }
+      },
+    []
+  ); /** END MENUANIMATION CLASS*/
+
+  const M = useMemo(
+    () =>
+      new MenuAnimation(
+        setMenuState,
+        menuState,
+        subMenuState,
+        setSubMenuState,
+        exitRef
+      ),
+    [
+      MenuAnimation,
+      setMenuState,
+      menuState,
+      subMenuState,
+      setSubMenuState,
+      exitRef,
+    ]
+  );
 
   const buttonEventHandler = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     menuState: MenuStateTypes
   ) => {
     e;
-    const m = new MenuAnimation(setMenuState, menuState);
-    await m.toggleMenu(menuState);
+    await M.toggleMenu(menuState);
   };
 
   return (
@@ -268,8 +321,7 @@ function NavMenu({
       <menu
         className={navCSS["menu"]}
         onClick={(e) => {
-          const m = new MenuAnimation(setMenuState, menuState);
-          m.mouseMenuEventHandlerFunc(e);
+          M.mouseMenuEventHandlerFunc(e);
         }}
         ref={menuRef}
       >
