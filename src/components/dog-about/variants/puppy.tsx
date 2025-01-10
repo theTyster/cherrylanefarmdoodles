9be89c0e196cd1f9bd0f40b6b90f486d398f54d0
Ty <noreply@mail.thetyster.dev>
2025-01-10 15:55:37 -0800

    Fix: Removes 'on' for strings containing 'Born/Due on'.

--- src/components/dog-about/variants/puppy.tsx
+++ src/components/dog-about/variants/puppy.tsx
@@ -56,7 +56,7 @@ export default function Puppy({
 
       if (birthday) {
         const birthdayObject = new Date(birthday);
-        hasPuppiesString.descriptor = `Born on ${normalizeEpochDate(
+        hasPuppiesString.descriptor = `Born ${normalizeEpochDate(
           birthday,
           "date-only"
         )}`;
@@ -73,7 +73,7 @@ export default function Puppy({
     }
     else if (birthday) {
         const birthdayObject = new Date(birthday);
-        hasPuppiesString.descriptor = `Born on ${normalizeEpochDate(
+        hasPuppiesString.descriptor = `Born ${normalizeEpochDate(
           birthday,
           "date-only"
         )}`;
