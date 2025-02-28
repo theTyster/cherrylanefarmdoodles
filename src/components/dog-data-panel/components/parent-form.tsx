1bb8184ec5535e3ede0f1c9f38890aa19aa8eac0
Ty <noreply@mail.thetyster.dev>
2025-02-28 11:59:46 -0800

    Fix: Makes refreshable-options component modular.

--- src/components/dog-data-panel/components/parent-form.tsx
+++ src/components/dog-data-panel/components/parent-form.tsx
@@ -1,22 +1,35 @@
-export const runtime = "edge";
-
+"use client";
+import { useState, useRef } from "react";
 import { GlobalNameSpaces as G } from "@/constants/data";
 
 // Utilities
 import { nameLength } from "@/components/dog-data-panel/constants/form-defaults";
-import RequiredStar from "@/components/dog-data-panel/components/required-star";
 import DogInputs from "@/components/dog-data-panel/components/inputs-for-all-dogs";
+import synchronizeCurrentData from "@/components/dog-data-panel/actions/synchronize-current-data";
+import { CURRENT_DATA } from "@/components/dog-data-panel//actions/admin-data-handler";
 
 //Components
 import BreederOptions from "@/components/dog-data-panel/components/refreshable-options";
+import RequiredStar from "@/components/dog-data-panel/components/required-star";
 import FormInput from "@/components/dog-data-panel/components/form-input";
-import Certifications from "@/components/dog-data-panel/components/refreshable-options";
+import CertificationOptions from "@/components/dog-data-panel/components/refreshable-options";
 import ImageUpload from "@/components/dog-data-panel/components/image-upload";
+import RefreshButton from "@/components/refreshButton/refresh-button";
+
+// Static
+import css from "@styles/dog-data-panel.module.scss";
 
 function ParentForm() {
+  // States
+  const [breeder, setBreeder] = useState("Cherry Lane Farms");
+  const [certification, setCertification] = useState("Embark");
+
+  // Refs
+  const certificationRefInput = useRef<HTMLInputElement>(null);
+  const breederRefInput = useRef<HTMLInputElement>(null);
+
   return (
     <>
-
       <FormInput label="Parent's Name (optional)">
         <input
           name={G.adultName}
@@ -30,7 +43,33 @@ function ParentForm() {
         <input name={G.adultBirthday} type="date" placeholder="MM/DD/YYYY" />
       </FormInput>
 
-      <BreederOptions whichOptions="breeders" />
+      {breeder !== "New" ? (
+        <>
+      <div className={css["input-with-button"]}>
+        <select name={G.breeder} required>
+          <BreederOptions whichOptions={CURRENT_DATA['breeders']} />
+          <option onClick={() => setBreeder("New")}>+ Add New</option>
+        </select>
+        <RefreshButton
+          refreshAction={async () => await synchronizeCurrentData(CURRENT_DATA['breeders'])}
+        />
+      </div>
+          </>
+      ) : (
+        <>
+          <input
+            name={G.breeder}
+            ref={breederRefInput}
+            type="text"
+            placeholder="Add the new breeder"
+            autoFocus={true}
+            required
+          />
+          <button type="button" onClick={() => setBreeder("Cherry Lane Farms")}>
+            Select From List Instead
+          </button>
+        </>
+      )}
 
       <FormInput
         label={
@@ -71,10 +110,37 @@ function ParentForm() {
       <FormInput label="Weight (optional)">
         <input name={G.weight} type="number" placeholder="Weight" />
       </FormInput>
-      <Certifications whichOptions="certifications" />
+      {certification !== "New" ? (
+        <>
+      <div className={css["input-with-button"]}>
+        <select name={G.certifications} required>
+          <option value={undefined}>None</option>
+          <CertificationOptions whichOptions="certificateNames" />
+          <option onClick={() => setCertification("New")}>+ Add New</option>
+        </select>
+        <RefreshButton
+          refreshAction={async () => await synchronizeCurrentData(CURRENT_DATA['certificateNames'])}
+        />
+      </div>
+          </>
+      ) : (
+        <>
+          <input
+            name={G.certifications}
+            ref={certificationRefInput}
+            type="text"
+            placeholder="Add the new certification"
+            autoFocus={true}
+            required
+          />
+          <button type="button" onClick={() => setCertification("Embark")}>
+            Select From List Instead
+          </button>
+        </>
+      )}
 
-      <ImageUpload variant='small'/>
-      <ImageUpload variant='large'/>
+      <ImageUpload variant="small" />
+      <ImageUpload variant="large" />
     </>
   );
 }
--- src/components/dog-data-panel/components/puppy-form.tsx
+++ src/components/dog-data-panel/components/puppy-form.tsx
@@ -5,6 +5,8 @@ import { GlobalNameSpaces as G } from "@/constants/data";
 
 // Utilities
 import { nameLength } from "@/components/dog-data-panel/constants/form-defaults";
+import { CURRENT_DATA } from "@/components/dog-data-panel//actions/admin-data-handler";
+import synchronizeCurrentData from "@/components/dog-data-panel/actions/synchronize-current-data";
 
 // Components
 import FormInput from "@/components/dog-data-panel/components/form-input";
@@ -12,8 +14,13 @@ import RequiredStar from "@/components/dog-data-panel/components/required-star";
 import DogInputs from "@/components/dog-data-panel/components/inputs-for-all-dogs";
 import LitterOptions from "@/components/dog-data-panel/components/refreshable-options";
 import ImageUpload from "@/components/dog-data-panel/components/image-upload";
+import RefreshButton from "@/components/refreshButton/refresh-button";
+
+// Static
+import css from "@styles/dog-data-panel.module.scss";
 
 function PuppyForm() {
+  const whichOptions = CURRENT_DATA['litterNames'];
   return (
     <>
       <FormInput label="Puppy Name (optional)">
@@ -41,10 +48,19 @@ function PuppyForm() {
         </select>
       </FormInput>
 
-      <LitterOptions whichOptions="litters" />
+      <FormInput label="What litter does this puppy belong to?">
+      <div className={css["input-with-button"]}>
+        <select name={G.litterId}>
+          <LitterOptions whichOptions={whichOptions} />
+        </select>
+        <RefreshButton
+          refreshAction={async () => await synchronizeCurrentData(whichOptions)}
+        />
+      </div>
+      </FormInput>
 
-      <ImageUpload variant='small'/>
-      <ImageUpload variant='large'/>
+      <ImageUpload variant="small" />
+      <ImageUpload variant="large" />
     </>
   );
 }
