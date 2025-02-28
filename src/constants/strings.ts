657b2122dbbca46cb865574732aa7c8483df644a
Ty <noreply@mail.thetyster.dev>
2025-02-28 10:46:43 -0800

    Feat: Create strings constants file.

---
+++ src/constants/strings.ts
@@ -0,0 +1,7 @@
+/**
+ * This file provides helpful strings that are used in multiple places.
+ * Often these strings are used in testing as well as building the application.
+ **/
+/***/
+export const errorText = "There was an error loading this component. Try Refreshing.";
+export const loadingText = "Please Wait...";
