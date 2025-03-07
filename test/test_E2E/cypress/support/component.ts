78c5bdf29fe9ffe177e363dfffe0b841261427c6
Ty <noreply@mail.thetyster.dev>
2025-03-07 11:24:17 -0800

    WIP: Adding browserstack for cross-platform testing?

--- test/test_E2E/cypress/support/component.ts
+++ test/test_E2E/cypress/support/component.ts
@@ -1,3 +1,5 @@
+//eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./component.d.ts" />
 // ***********************************************************
 // This example support/component.ts is processed and
 // loaded automatically before your test files.
@@ -17,18 +19,6 @@
 import './commands'
 
 import { mount } from 'cypress/react'
-
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount
-    }
-  }
-}
 
 Cypress.Commands.add('mount', mount)
 
